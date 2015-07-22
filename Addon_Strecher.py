3db37348d96d665f60d6d4b088f8b8d3756bf377
Randrian <Randrian@localhost>
2015-07-22 11:31:22 +0200

    * removed debug logs in Addon_Strecher and cleaned code

--- Addon_Strecher.py
+++ Addon_Strecher.py
@@ -2,55 +2,31 @@
 def OverloadImageRead(func):
     def wrapper(*args, **kwargs):
         name = args[1]
-        image1= func(*args, **kwargs)
+        image1 = func(*args, **kwargs)
         image = np.zeros((image1.shape[0], image1.shape[1], 3))
         if name.find("mask") != -1:
             return image
-        #image[:,:,0] = image1
-        #image[:,:,1] = image1
-        #image[:,:,2] = image1
-        print("Overloading",name)
         if name.find("BF") != -1 and name.find("mask") == -1:
-            print("Overloaded")
             name2 = name.replace("BF","Fluo1")
             if os.path.exists(name2) and 1:
-                #args[1] = name2
                 image2 = func(args[0], name2, **kwargs)
-                print np.amax(image2)
                 image2 = image2/np.amax(image2)
-                #image2 = image2**2
                 image2 = image2*256
-                alhpa = image2/256.
-                print np.amax(image2)
-                #image[:,:,2] = (image[:,:,2]*(1-alhpa))+(image2*alhpa)
                 image[:,:,1] = image2
             name2 = name.replace("BF","Fluo1")
             name2 = os.path.join(srcpath2, os.path.split(name2)[1])
-            print("----",name2)
             if os.path.exists(name2) and 1:
-                #args[1] = name2
                 image2 = func(args[0], name2, **kwargs)
                 image2 = image2/np.amax(image2)
-                #image2 = image2**2
                 image2 = image2*256
-                alhpa = image2/256.
-                print(np.amax(image2))
-                #image[:,:,0] = (image[:,:,0]*(1-alhpa))+(image2*alhpa)
                 image[:,:,2] = image2
             name2 = name.replace("BF","Fluo2")
             name2 = os.path.join(srcpath2, os.path.split(name2)[1])
-            print("----",name2)
             if os.path.exists(name2) and 1:
-                #args[1] = name2
                 image2 = func(args[0], name2, **kwargs)
                 image2 = image2/np.amax(image2)
-                #image2 = image2**2
                 image2 = image2*256
-                alhpa = image2/256.
-                print np.amax(image2)
-                #image[:,:,0] = (image[:,:,0]*(1-alhpa))+(image2*alhpa)
                 image[:,:,0] = image2
-#            image[:,:,2] = 0
         return image
     return wrapper
 
@@ -72,8 +48,6 @@ def OverloadKeyPressEvent(func):
                 fp.write(str(sum(data[:,2]==1))+";")
                 fp.write(str(sum(data[:,2]==2))+";\n")    
             fp.close()
-                
-                
             return
         if event.key() == QtCore.Qt.Key_Left:
             self_class.SaveMaskAndPoints()
