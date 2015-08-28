c271795716adecdec647118ded366feb6c2f7865
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2015-08-28 14:55:54 +0200

    * code cleanup

--- annotationoverview.py
+++ annotationoverview.py
@@ -23,11 +23,11 @@ class AnnotationOverview(QWidget):
         self.mh=mediahandler
         self.frameSlider = frameSlider
 
+        self.frame_list = [os.path.split(file)[1][:-4] for file in self.mh.filelist]
+
         # get list of files
         input = os.path.join(self.outputpath, '*' + self.defsuffix)
         self.filelist = natsorted(glob.glob(input))
-        print self.filelist
-
 
         # widget layout and ellements
         self.setMinimumWidth(700)
@@ -35,12 +35,10 @@ class AnnotationOverview(QWidget):
         self.setWindowTitle('Annotations')
         self.layout = QGridLayout(self)
 
-        self.table= QTableWidget(self)
+        self.table= QTableWidget(0, 7, self)
         self.table.setEditTriggers(QTableWidget.NoEditTriggers)
-        self.table.setColumnCount(6)
-        self.table.setRowCount(0)
-        self.table.setHorizontalHeaderLabels(QStringList(['Date','Tag','Comment','R','System','Cam']))
-        #self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.setHorizontalHeaderLabels(QStringList(['Date','Tag','Comment','R','System','Cam','file']))
+        self.table.hideColumn(6)
         self.table.horizontalHeader().setResizeMode(QHeaderView.ResizeToContents)
         self.table.horizontalHeader().setResizeMode(2,QHeaderView.Stretch)
         self.table.verticalHeader().hide()
@@ -50,80 +48,56 @@ class AnnotationOverview(QWidget):
 
         for i,file in enumerate(self.filelist):
             # read annotation file
-            results,comment=ReadAnnotation(file)
+            results, comment = ReadAnnotation(file)
 
-            # backward compatibility for old files
-            if results['timestamp']=='':
-                path,fname=os.path.split(file)
-                results['timestamp'] = fname[0:15]
-            if comment=='' and results['tags']=='':
-                f=QFile(file)
-                f.open(QFile.ReadWrite)
-                inS = QTextStream(f)
-                comment=inS.readAll()
-                f.close()
-
+            # get file basename
+            filename = os.path.split(file)[1]
+            basename = filename[:-len(self.defsuffix)]
 
             # populate table
-            ti = QTableWidgetItem()
-            ti.setText(results['timestamp'])
             self.table.insertRow(self.table.rowCount())
-            self.table.setItem(i,0,ti)
+            self.table.setItem(i,0,QTableWidgetItem(results['timestamp']))
+            self.table.setItem(i,1,QTableWidgetItem(", ".join(results['tags'])))
+            self.table.setItem(i,2,QTableWidgetItem(comment))
+            self.table.setItem(i,3,QTableWidgetItem(str(results['rating'])))
+            self.table.setItem(i,4,QTableWidgetItem(results['system']))
+            self.table.setItem(i,5,QTableWidgetItem(results['camera']))
+            self.table.setItem(i,6,QTableWidgetItem(basename))
+            print(basename)
 
-            ti = QTableWidgetItem()
-            ti.setText(", ".join(results['tags']))
-            self.table.insertRow(self.table.rowCount())
-            self.table.setItem(i,1,ti)
-
-            ti = QTableWidgetItem()
-            ti.setText(comment)
-            self.table.insertRow(self.table.rowCount())
-            self.table.setItem(i,2,ti)
-
-            ti = QTableWidgetItem()
-            ti.setText(str(results['rating']))
-            self.table.insertRow(self.table.rowCount())
-            self.table.setItem(i,3,ti)
-
-            ti = QTableWidgetItem()
-            ti.setText(str(results['system']))
-            self.table.insertRow(self.table.rowCount())
-            self.table.setItem(i,4,ti)
-
-            ti = QTableWidgetItem()
-            ti.setText(str(results['camera']))
-            self.table.insertRow(self.table.rowCount())
-            self.table.setItem(i,5,ti)
-
-            jtidx=None
-            for i,file in enumerate(self.mh.filelist):
-                path,fname=os.path.split(file)
-                if fname[0:15]==results['timestamp']:
-                    jtidx=i
-                    break
-            if jtidx != None:
-                self.frameSlider.addTickMarker(jtidx)
-
+            try:
+                index = self.frame_list.index(basename)
+            except:
+                pass
+            else:
+                self.frameSlider.addTickMarker(index)
 
         # fit column to context
         self.table.resizeColumnToContents(0)
 
+        logname_tag = "_pos.txt"
+        input = os.path.join(self.outputpath, '*' + logname_tag)
+        marker_filelist = natsorted(glob.glob(input))
+        for file in marker_filelist:
+            filename = os.path.split(file)[1]
+            basename = filename[:-len(logname_tag)]
+            try:
+                index = self.frame_list.index(basename)
+            except:
+                pass
+            else:
+                print index
+                self.frameSlider.addTickMarker(index, QColor("green"))
 
     def JumpToAnnotation(self,idx):
-        #print "double clicked"
-        #print idx.row()
-        timestamp= self.table.item(idx.row(),0).text()
+        # get file basename
+        basename = self.table.item(idx.row(),6).text()
 
-        # find match in mh file list
-        jtidx=[]
-        for i,file in enumerate(self.mh.filelist):
-            path,fname=os.path.split(file)
-            if fname[0:15]==timestamp:
-                jtidx=i
-                break
-        if jtidx==[]:
-            print 'no matching file found for ts: %s'%timestamp
+        # find match in file list
+        try:
+            index = self.frame_list.index(basename)
+        except:
+            print 'no matching file found for '+basename
         else:
-            print 'got to file: %s'% self.mh.filelist[jtidx]
-            self.window.JumpToFrame(jtidx)
+            self.window.JumpToFrame(index)
 
