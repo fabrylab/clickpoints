219f74cff3d85958870172910bd9a7fe36a17c1c
Randrian <Randrian@localhost>
2015-09-16 16:45:38 +0200

    * PathWalker: allow sub second timestamps

--- database/PathWalker.py
+++ database/PathWalker.py
@@ -3,14 +3,15 @@ import os
 import re
 import glob
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from database import Database
 
 def AddPathToDatabase(root):
     root = os.path.normpath(root)
     folder_list = [x for x in root.split("\\") if x != ""]
     parent_id = 0
-    for folder in folder_list[:-1]:
+    print(folder_list)
+    for folder in folder_list[:]:
         parent_id = database.savePath(folder, parent_id)
     return parent_id
 
@@ -23,17 +24,20 @@ device_name = "microbs31_3"
 
 #files = glob.glob(r"\\131.188.117.94\spot2014\campbell\2014\*\*\*SPOT_CAMP.jpg")
 start_path = r"\\131.188.117.94\spot2014\mobotix\2014"
+start_path = r"\\131.188.117.94\spot2014\mobotix_south\2014"
+start_path = r"\\131.188.117.94\spot2014\ge"
 #files = glob.glob(r"\\131.188.117.94\spot2014\mobotix\2014\*\*\*SPOT_mobotix.jpg")
-filename_data_regex = r'.*(?P<timestamp>\d{8}-\d{6})_(?P<system>.+?[^_])_(?P<device>.+)\.jpg'
+filename_data_regex = r'.*(?P<timestamp>\d{8}-\d{6})-(?P<micros>\d)_(?P<system>.+?[^_])_(?P<device>.+)\.jpg'
 time_format = '%Y%m%d-%H%M%S'
 system_name = "AtkaSpot"
 device_name = "Campbell"
-device_name = "Mobotix"
+device_name = "GE4000"
 last_folder = ""
 
 for root, dirs, files in os.walk(start_path):
     print(root, files)
     folder_id = AddPathToDatabase(root)
+    print("folder_id", folder_id)
     query = database.SQL_Files.delete().where(database.SQL_Files.path == folder_id)
     print(query.execute(),"items deleted")
     file_data = []
@@ -44,6 +48,7 @@ for root, dirs, files in os.walk(start_path):
             continue
         data = match.groupdict()
         tstamp = datetime.strptime(data["timestamp"], time_format)
+        tstamp = tstamp + timedelta(microseconds=int(data["micros"])*1e5)
         system_id = database.getSystemId(system_name)
         device_id = database.getDeviceId(system_name, device_name)
         file_data.append(dict(timestamp=tstamp, system=system_id, device=device_id, basename=basename, path=folder_id, extension=ext))
