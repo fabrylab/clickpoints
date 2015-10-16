dbba36056c229fda09ebf8d250555d27077101fd
Randrian <Randrian@localhost>
2015-10-16 19:29:41 +0200

    * extended the linux installation to feature folders, startup menu, terminal command

--- install_bat.py
+++ install_bat.py
@@ -1,7 +1,8 @@
 import sys, os
 
-script_path = os.path.join(os.path.dirname(__file__), "ClickPointsQT.py")
-icon_path = os.path.join(os.path.dirname(__file__), "icons", "ClickPoints.ico")
+directory = os.path.dirname(os.path.abspath(__file__))
+script_path = os.path.join(directory, "ClickPointsQT.py")
+icon_path = os.path.join(directory, "icons", "ClickPoints.ico")
 if sys.platform.startswith('win'):
     with open("ClickPoints.bat", 'w') as fp:
         print("Writing ClickPoints.bat")
@@ -11,23 +12,32 @@ if sys.platform.startswith('win'):
         fp.write(" -srcpath=%1\n")
         fp.write("IF %ERRORLEVEL% NEQ 0 pause\n")
 else:
-    with open("ClickPoints.sh", 'w') as fp:
-        print("Writing ClickPoints.sh")
+    sh_file = os.path.join(directory, "ClickPoints")
+    with open(sh_file, 'w') as fp:
+        print("Writing ClickPoints bash file")
         fp.write("#!/bin/bash\n")
         fp.write("python")
         fp.write(" ")
         fp.write(script_path)
         fp.write(" -srcpath=\"$1\"\n")
+        os.system("chmod +x %s" % sh_file)
         
-    with open("/home/"+os.popen('whoami').read()[:-1]+"/.local/share/applications/ClickPoints.desktop", 'w') as fp:
+    print("Copying ClickPoints bash file to /bin/")
+    os.popen("sudo cp %s /bin/" % sh_file)
+        
+    desktop_file = "/home/"+os.popen('whoami').read()[:-1]+"/.local/share/applications/ClickPoints.desktop"
+    with open(desktop_file, 'w') as fp:
         print("Writing ClickPoints.desktop")
         fp.write("[Desktop Entry]\n")
         fp.write("Type=Application\n")
         fp.write("Name=ClickPoints\n")
-        fp.write("GenericName=Image View and Annotate\n")
+        fp.write("GenericName=View Images/Videos and Annotate them\n")
         fp.write("Comment=Display images and videos and annotate them\n")
         fp.write("Exec=python "+script_path+" -srcpath=\"%f\"\n")
-        fp.write("NoDisplay=true\n")
+        fp.write("NoDisplay=false\n")
+        fp.write("Terminal=false\n")
         fp.write("Icon="+icon_path+"\n")
-        fp.write("Categories=Graphics;2DGraphics;RasterGraphics;Video;Qt;\n")
-        fp.write("MimeType=video/x-msvideo;video/mpeg;image/bmp;image/png;image/jpeg;image/tiff;image/gif;$\n")
+        fp.write("Categories=Development;Science;IDE;Qt;\n")
+        fp.write("MimeType=inode/directory;video/mp4;video/x-msvideo;video/mpeg;image/bmp;image/png;image/jpeg;image/tiff;image/gif;$\n")
+        fp.write("InitialPreference=10\n")
+        
