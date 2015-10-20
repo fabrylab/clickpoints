79920831462e3eb0ca7d423bded1066b2eeff87a
Fox <Fox@localhost>
2015-10-20 17:32:51 +0200

    + added install script for win clickpoints

---
+++ install_clickpoints.bat
@@ -0,0 +1,12 @@
+@echo off
+::install super script for clickpoints
+::assuming python was added to system PATH variable
+
+::generate Clickpoints.bat
+python install_bat.py 
+echo DONE
+
+::generate entries in w indows registry
+echo Register Clickpoints ...
+python install_registry.py install
+echo DONE
