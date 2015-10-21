1d5860eabb16461aa7a7d6f974a74abc39fa7167
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2015-10-21 16:19:01 +0200

    + added doc comments to ConfigLoad.py

--- ConfigLoad.py
+++ ConfigLoad.py
@@ -9,61 +9,87 @@ except ImportError:
 
 start_globals = globals().copy()
 
-""" General """
+""" @config General """
 srcpath = ""
+# @config `outputpath =` the path where to save the log and mask file. Set to `None` defaults to the `srcpath`
 outputpath = ""
+# @config `filename_data_regex = ` specify a regular expression to obtain meta-data from filenames
 filename_data_regex = r'.*(?P<timestamp>\d{8}-\d{6})_(?P<system>.+?[^_])_(?P<camera>.+)'
+# @config `filterparam =` specify additional filters for the files to use
 filterparam = {}
 
+# @config `jumps =` specify how many frames the numpad keys `2`,`3`,`5`,`6`,`8`,`9`,`/`,`*` should jump
 jumps = (-1, +1, -10, +10, -100, +100, -1000, +1000)
 
+# @config `max_image_size = ` the maximum size of one image before it is internally split into tiles
 max_image_size = 2 ** 12
 
+# @config `rotation =` the rotation of the image when starting
 rotation = 0
+# @config `rotation_steps =` how much to rotate time image when `r` is pressed
 rotation_steps = 90
 
+# @config `hide_interfaces =` whether to hide the interfaces a program start. Press F2 to show it again.
 hide_interfaces = True
 
+# @config `addons =` a list of additional python files to load
 addons = []
 
-""" Marker """
+""" @config Marker """
+# @config `logname_tag =` specifies what to append to the log file.
 logname_tag = '_pos.txt'
 
 TYPE_Normal = 0
 TYPE_Rect = 1
 TYPE_Line = 2
 
+# @config `types = {0: ["marker", [255, 0, 0], TYPE_Normal]}` specifies what categories to use. Every category is an array with three entires. Name, Color and Type. Types can be 0: normal marker, 1: rectangle markers, 2: line markers
 types = {0: ["marker", [255, 0, 0], TYPE_Normal]}
 
+# @config `tracking = ` specify whether to use tracking mode
 tracking = False
+# @config `tracking_connect_nearest =` if set to true, a new marker will always be connected to the nearest track
 tracking_connect_nearest = False
+# @config `tracking_show_trailing =` how many track points to show before the current frame, to show all use -1
 tracking_show_trailing = -1
+# @confing `tracking_show_leading =` how many track points to show after the current frame, to show all use -1
 tracking_show_leading = 0
 
-""" Mask """
+""" @config Mask """
+# @config `maskname_tag =` specifies what to append to the mask file.
 maskname_tag = '_mask.png'
+# @config `auto_mask_update =` whether to update the mask display after each stroke or manually by key press
 auto_mask_update = True
+# @config `draw_types = [[0,[255,0,0]]` specifies what categories to use for mask drawing. Every category is an array with two entires. Index and Color.
 draw_types = [[0, (0, 0, 0)],
               [255, [255, 255, 255]],
               [124, [124, 124, 255]]]
 
-""" GammaCorrection """
+""" @config GammaCorrection """
 
 
-""" Timeline """
+""" @config Timeline """
+# @config `fps =` if not 0 overwrite the frame rate of the video
 fps = 0
+# @config `play_start =` at which frame to start playback (if > 1) or at what fraction of the video to start playback (if > 0 and < 1)
 play_start = 0.0
+# @config `playing =` whether to start playback at the program start
 play_end = 1.0
+# @config `playing =` whether to start playback at the program start
 playing = False
+# @config `timeline_hide =` whether to hide the timeline at the program start
 timeline_hide = False
 
-""" Annotations """
+""" @config Annotations """
+# @config `annotation_tag =` specifies what to append to the annotation file.
 annotation_tag = '_annot.txt'
 
-""" FolderBrowser """
+""" @config FolderBrowser """
+# @config `folder_list =` specify a lists of folder which can be switched with the `path_up` and `page_down` keys.
 folder_list = []
 
-""" ScriptLauncher """
+""" @config ScriptLauncher """
+# @config `launch_scripts = ` specify a list of scripts which can be started by pressing `F12` to `F9`
 launch_scripts = []
 
 # enables .access on dicts
