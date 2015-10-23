e15dad783a0a536c59776a89ff4b84dd46a1149a
Fox <57496278+foxsr@users.noreply.github.com>
2015-10-23 11:25:13 +0200

    + added and deactivated add ClickPoints path to user PATH

--- install_clickpoints.bat
+++ install_clickpoints.bat
@@ -5,10 +5,21 @@
 ::generate Clickpoints.bat
 python install_bat.py 
 echo DONE
+echo.
+
+:: register ClickPoints PATH to user PATH variable
+:: for convinient CMD acces via ClickPoints.bat
+::echo Adding ClickPoints to User PATH
+::SET basepath=%~dp0
+::IF %basepath:~-1%==\ SET basepath=%basepath:~0,-1%
+::setx path "%PATH%";"%basepath%"
+::echo DONE
+::echo.
 
 ::generate entries in w indows registry
 echo Register Clickpoints ...
 python install_registry.py install
 echo DONE
+echo.
 
 pause
