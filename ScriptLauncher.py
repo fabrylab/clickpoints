9eac6dd12eea75cfe7f2e7de512b71148464f749
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2015-10-27 23:45:32 +0100

    + added server communication to ScriptLauncher

--- ScriptLauncher.py
+++ ScriptLauncher.py
@@ -3,23 +3,54 @@ import os, sys
 
 try:
     from PyQt5 import QtCore
+    from PyQt5.QtCore import pyqtSignal, QThread, QObject
 except ImportError:
     from PyQt4 import QtCore
+    from PyQt4.QtCore import pyqtSignal, QThread, QObject
 
-class ScriptLauncher:
+import SocketServer, socket, threading, subprocess
+
+class ThreadedUDPRequestHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        self.server.signal.emit(self.request[0])
+
+class ScriptLauncher(QObject):
+    signal = pyqtSignal(str)
+
     def __init__(self, window, media_handler, config=None):
+        QObject.__init__(self)
         self.window = window
         self.media_handler = media_handler
         self.config = config
 
-    def keyPressEvent(self, event):
+        self.HOST, self.PORT = "localhost", 55005
+        # Try to connect the server at the next free port
+        while True:
+            try:
+                server = SocketServer.ThreadingUDPServer((self.HOST, self.PORT), ThreadedUDPRequestHandler)
+            except socket.error:
+                self.PORT += 1
+            else:
+                break
 
+        server.signal = self.signal
+        server.signal.connect(self.Command)
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+
+    def Command(self, command):
+        cmd, value = str(command).split(" ", 1)
+        if cmd == "JumpFrames":
+            self.window.JumpFrames(int(value))
+
+    def keyPressEvent(self, event):
         keys = [QtCore.Qt.Key_F12, QtCore.Qt.Key_F11, QtCore.Qt.Key_F10, QtCore.Qt.Key_F9, QtCore.Qt.Key_F8, QtCore.Qt.Key_F7, QtCore.Qt.Key_F6, QtCore.Qt.Key_F5]
         for script, key in zip(self.config.launch_scripts, keys):
             # @key F12: Launch
             if event.key() == key:
                 self.window.save()
-                os.system(r"%s %s %s %d" % (sys.executable, script, os.path.abspath(self.config.srcpath), self.media_handler.currentPos))
+                subprocess.Popen([sys.executable, os.path.abspath(script), os.path.abspath(self.config.srcpath), str(self.media_handler.currentPos), str(self.PORT)])
 
     @staticmethod
     def file():
