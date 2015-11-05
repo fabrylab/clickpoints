a3cdc59892d06f8d672a79524f9fdb4a7cfd79a1
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2015-11-05 14:13:43 +0100

    + added tools as python package "clickpoints"

---
+++ tools/clickpoints/SendCommands.py
@@ -0,0 +1,21 @@
+import socket, sys
+
+def send(message):
+    HOST, PORT = "localhost", int(sys.argv[3])
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message, (HOST, PORT))
+
+def send2(message):
+    HOST, PORT = "localhost", int(sys.argv[3])
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message, (HOST, PORT))
+    return sock.recv(1024)
+
+def JumpFrames(value):
+    send("JumpFrames %d \n" % value)
+
+def GetImageName(value):
+    return send2("GetImageName %d \n" % value)
+
+def GetMarkerName(value):
+    return send2("GetMarkerName %d \n" % value)
---
+++ tools/clickpoints/__init__.py
@@ -0,0 +1,2 @@
+import SendCommands
+import MarkerLoad
