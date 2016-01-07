1712d0f0a3913a5e160b4b3503778bcbd28c152d
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2016-01-07 17:02:43 +0100

    ! added includes path to modules/_init_.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "includes"))
 from MaskHandler import MaskHandler
 from MarkerHandler import MarkerHandler
 from Timeline import Timeline
