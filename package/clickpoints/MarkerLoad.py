8911212c1e156bec6986749f4965413c25df65e5
Fox <57496278+foxsr@users.noreply.github.com>
2016-02-03 09:32:05 +0100

    + ML: added error checking for missing/nonexisting db file

--- package/clickpoints/MarkerLoad.py
+++ package/clickpoints/MarkerLoad.py
@@ -10,6 +10,10 @@ from PIL import Image
 class DataFile:
     def __init__(self, database_filename='clickpoints.db'):
         self.database_filename = database_filename
+
+        if not os.path.exists(self.database_filename):
+            raise Exception("DB %s does not exist!" % os.path.abspath(self.database_filename))
+
         self.db = apsw_ext.APSWDatabase(database_filename)
 
         """ Basic Tables """
@@ -79,217 +83,212 @@ class DataFile:
         self.db.connect()
 
 if os.path.exists('clickpoints.db'):
-    database = DataFile()
 
-def GetImages():
-    """
-    Get all images sorted by filename
+    
+    def GetImages(self):
+        """
+        Get all images sorted by filename
+    
+        Returns
+        -------
+        entries : array_like
+            a query object containing all the images in the database file.
+        """
 
-    Returns
-    -------
-    entries : array_like
-        a query object containing all the images in the database file.
-    """
-    global database
-    query = database.table_images.select()
-    query.order_by(database.table_images.filename)
-    return query
+        query = self.table_images.select()
+        query.order_by(self.table_images.filename)
+        return query
+    
+    def GetTracks(self):
+        """
+        Get all track entries
+    
+        Returns
+        -------
+        entries : array_like
+            a query object which contains the requested tracks.
+        """
 
-def GetTracks():
-    """
-    Get all track entries
-
-    Returns
-    -------
-    entries : array_like
-        a query object which contains the requested tracks.
-    """
-    global database
-    query = database.table_tracks.select()
-    return query
-
-def GetTypes():
-    """
-    Get all type entries
-
-    Returns
-    -------
-    entries : array_like
-        a query object which contains all marker types.
-    """
-    global database
-    query = database.table_types.select()
-    return query
-
-def GetMarker(image=None, image_frame=None, processed=None, type=None, type_name=None, track=None):
-    """
-    Get all the marker entries in the database where the parameters fit. If a parameter is omitted, the column is
-    ignored. If it is provided a single value, only database entries matching this value are returned. If a list is
-    supplied, any entrie with a value from the list is machted.
-
-    Parameters
-    ----------
-    image : int, array, optional
-        the image id(s) for the markers to be selected.
-    image_frame : int, array, optional
-        the image frame(s) for the markers to be selected.
-    processed : bool, array, optional
-        the processed flag(s) for the markers to be selected.
-    type : int, array, optional
-        the type id(s) for the markers to be selected.
-    track : int, array, optional
-        the track id(s) for the markers to be selected.
-
-    Returns
-    -------
-    entries : array_like
-        a query object which can be iterated to get the track entries wich where machted by the paremeters provided.
-    """
-    global database
-    # join marker and type table
-    query = query=database.table_marker.select(database.table_marker,database.table_types).join(database.table_types,peewee.JOIN.LEFT_OUTER)
-    parameter = [image, image_frame, processed, type, type_name, track]
-    table = database.table_marker
-    fields = [table.image, table.image_frame, table.processed, table.type, database.table_types.name, table.track]
-    for field, parameter in zip(fields, parameter):
-        if parameter is None:
-            continue
-        if isinstance(parameter, (tuple, list)):
-            query = query.where(field << parameter)
-        else:
-            query = query.where(field == parameter)
-    return query
-
-def SetMarker(id=None, image=None, image_frame=None, x=None, y=None, processed=None, partner=None, type=None, track=None):
-    """
-    Insert or update markers in the database file. Every parameter can either be omitted, to use the default value,
-    supplied with a single value, to use the same value for all entries, or be supplied with a list of values, to use a
-    different value for each entry. If any parameter is a list, multiple entries are inserted, otherwise just a single
-    value will be inserted or updated.
-
-    Parameters
-    ----------
-    id : int, array, optional
-        the id(s) for the markers to be inserted.
-    image : int, array, optional
-        the image id(s) for the markers to be inserted.
-    image_frame : int, array, optional
-        the image frame(s) for the markers to be inserted.
-    x : float, array, optional
-        the x coordinate(s) for the markers to be inserted.
-    y : float, array, optional
-        the y coordinate(s) for the markers to be inserted.
-    processed : bool, array, optional
-        the processed flag(s) for the markers to be inserted.
-    partner : int, array, optional
-        the partner id(s) for the markers to be inserted.
-    type : int, array, optional
-        the type id(s) for the markers to be inserted.
-    track : int, array, optional
-        the track id(s) for the markers to be inserted.
-    """
-    global database
-    data_sets = []
-    table = database.table_marker
-    fields = [table.id, table.image, table.image_frame, table.x, table.y, table.processed, table.partner_id, table.type, table.track]
-    names = ["id", "image_id", "image_frame", "x", "y", "processed", "partner_id", "type_id", "track_id"]
-    for data in np.broadcast(id, image, image_frame, x, y, processed, partner, type, track):
-        data_set = []
-        condition = "WHERE image_id = %d AND image_frame = %d AND track_id = %d" % (data[1], data[2], data[-1])
-        for field, name, value in zip(fields, names, data):
-            if value is None:
-                data_set.append("(SELECT "+name+" FROM marker "+condition+")")
+        query = self.table_tracks.select()
+        return query
+    
+    def GetTypes(self):
+        """
+        Get all type entries
+    
+        Returns
+        -------
+        entries : array_like
+            a query object which contains all marker types.
+        """
+        query = self.table_types.select()
+        return query
+    
+    def GetMarker(self,image=None, image_frame=None, processed=None, type=None, type_name=None, track=None):
+        """
+        Get all the marker entries in the database where the parameters fit. If a parameter is omitted, the column is
+        ignored. If it is provided a single value, only database entries matching this value are returned. If a list is
+        supplied, any entrie with a value from the list is machted.
+    
+        Parameters
+        ----------
+        image : int, array, optional
+            the image id(s) for the markers to be selected.
+        image_frame : int, array, optional
+            the image frame(s) for the markers to be selected.
+        processed : bool, array, optional
+            the processed flag(s) for the markers to be selected.
+        type : int, array, optional
+            the type id(s) for the markers to be selected.
+        track : int, array, optional
+            the track id(s) for the markers to be selected.
+    
+        Returns
+        -------
+        entries : array_like
+            a query object which can be iterated to get the track entries wich where machted by the paremeters provided.
+        """
+        # join marker and type table
+        query = query=self.table_marker.select(self.table_marker,self.table_types).join(self.table_types,peewee.JOIN.LEFT_OUTER)
+        parameter = [image, image_frame, processed, type, type_name, track]
+        table = self.table_marker
+        fields = [table.image, table.image_frame, table.processed, table.type, self.table_types.name, table.track]
+        for field, parameter in zip(fields, parameter):
+            if parameter is None:
+                continue
+            if isinstance(parameter, (tuple, list)):
+                query = query.where(field << parameter)
             else:
-                data_set.append(str(value))
-        data_sets.append(",\n ".join(data_set))
-    query = "INSERT OR REPLACE INTO marker (id, image_id, image_frame, x, y, processed, partner_id, type_id, track_id)\n VALUES (\n"
-    query += "),\n (".join(data_sets)
-    query += ");"
-    while 1:
+                query = query.where(field == parameter)
+        return query
+    
+    def SetMarker(self,id=None, image=None, image_frame=None, x=None, y=None, processed=None, partner=None, type=None, track=None):
+        """
+        Insert or update markers in the database file. Every parameter can either be omitted, to use the default value,
+        supplied with a single value, to use the same value for all entries, or be supplied with a list of values, to use a
+        different value for each entry. If any parameter is a list, multiple entries are inserted, otherwise just a single
+        value will be inserted or updated.
+    
+        Parameters
+        ----------
+        id : int, array, optional
+            the id(s) for the markers to be inserted.
+        image : int, array, optional
+            the image id(s) for the markers to be inserted.
+        image_frame : int, array, optional
+            the image frame(s) for the markers to be inserted.
+        x : float, array, optional
+            the x coordinate(s) for the markers to be inserted.
+        y : float, array, optional
+            the y coordinate(s) for the markers to be inserted.
+        processed : bool, array, optional
+            the processed flag(s) for the markers to be inserted.
+        partner : int, array, optional
+            the partner id(s) for the markers to be inserted.
+        type : int, array, optional
+            the type id(s) for the markers to be inserted.
+        track : int, array, optional
+            the track id(s) for the markers to be inserted.
+        """
+        data_sets = []
+        table = self.table_marker
+        fields = [table.id, table.image, table.image_frame, table.x, table.y, table.processed, table.partner_id, table.type, table.track]
+        names = ["id", "image_id", "image_frame", "x", "y", "processed", "partner_id", "type_id", "track_id"]
+        for data in np.broadcast(id, image, image_frame, x, y, processed, partner, type, track):
+            data_set = []
+            condition = "WHERE image_id = %d AND image_frame = %d AND track_id = %d" % (data[1], data[2], data[-1])
+            for field, name, value in zip(fields, names, data):
+                if value is None:
+                    data_set.append("(SELECT "+name+" FROM marker "+condition+")")
+                else:
+                    data_set.append(str(value))
+            data_sets.append(",\n ".join(data_set))
+        query = "INSERT OR REPLACE INTO marker (id, image_id, image_frame, x, y, processed, partner_id, type_id, track_id)\n VALUES (\n"
+        query += "),\n (".join(data_sets)
+        query += ");"
+        while 1:
+            try:
+                self.db.execute_sql(query)
+            except apsw.BusyError:
+                time.sleep(0.01)
+                continue
+            else:
+                break
+    
+    def GetMask(self,image, image_frame=0):
+        """
+        Get the mask image data for the image with the id `image`. If the database already has an entry the corresponding
+        mask will be loaded, otherwise a new empty mask array will be created.
+    
+        To save the changes on the mask use `SetMask`.
+    
+        Parameters
+        ----------
+        image : int
+            image id.
+        image_frame : int, optional
+            image frame number (default=0).
+    
+        Returns
+        -------
+        mask : ndarray
+            mask data for the image.
+        """
         try:
-            database.db.execute_sql(query)
-        except apsw.BusyError:
-            time.sleep(0.01)
-            continue
-        else:
-            break
-
-def GetMask(image, image_frame=0):
-    """
-    Get the mask image data for the image with the id `image`. If the database already has an entry the corresponding
-    mask will be loaded, otherwise a new empty mask array will be created.
-
-    To save the changes on the mask use `SetMask`.
-
-    Parameters
-    ----------
-    image : int
-        image id.
-    image_frame : int, optional
-        image frame number (default=0).
-
-    Returns
-    -------
-    mask : ndarray
-        mask data for the image.
-    """
-    global database
-    try:
-        # Test if mask already exists in database
-        mask_entry = database.table_mask.get(database.table_mask.image == image, database.table_mask.image_frame == image_frame)
-        # Load it
-        im = np.asarray(Image.open(mask_entry.filename))
-        im.setflags(write=True)
-        return im
-    except peewee.DoesNotExist:
-        # Create new mask according to image size
-        image_entry = database.table_images.get(database.table_images.id == image)
-        pil_image = Image.open(image_entry.filename)
-        im = np.zeros(pil_image.size, dtype=np.uint8)
-        return im
-
-def SetMask(mask, image, image_frame=0):
-    """
-    Add or overwrite the mask file for the image with the id `image` and the frame number `image_frame`
-
-    Parameters
-    ----------
-    mask : array_like
-        mask image data.
-    image : int
-        image id.
-    image_frame : int, optional
-        image frame number (default=0).
-    """
-    global database
-    try:
-        # Test if mask already exists in database
-        mask_entry = database.table_mask.get(database.table_mask.image == image, database.table_mask.image_frame == image_frame)
-        filename = mask_entry.filename
-    except peewee.DoesNotExist:
-        # Create new entry
-        mask_entry = database.table_mask(image=image, image_frame=image_frame)
-        # Get mask image name
-        image_entry = database.table_images.get(id=image)
-        if image_entry.frames > 1:
-            number = "_"+("%"+"%d" % np.ceil(np.log10(image_entry.frames))+"d") % image_frame
-        else:
-            number = ""
-        basename, ext = os.path.splitext(image_entry.filename)
-        directory, basename = os.path.split(basename)
-        current_maskname = os.path.join(directory, basename + "_" + ext[1:] + number + "_mask.png")
-        filename = current_maskname
-        # Save entry
-        mask_entry.filename = current_maskname
-        mask_entry.save()
-
-    # Create image
-    pil_image = Image.fromarray(mask)
-    # Create color palette
-    lut = np.zeros(3 * 256, np.uint8)
-    for draw_type in database.table_maskTypes.select():
-        index = draw_type.index
-        lut[index * 3:(index + 1) * 3] = [int("0x"+draw_type.color[i:i+2], 16) for i in [1, 3, 5]]
-    pil_image.putpalette(lut)
-    # Save mask
-    pil_image.save(filename)
+            # Test if mask already exists in database
+            mask_entry = self.table_mask.get(self.table_mask.image == image, self.table_mask.image_frame == image_frame)
+            # Load it
+            im = np.asarray(Image.open(mask_entry.filename))
+            im.setflags(write=True)
+            return im
+        except peewee.DoesNotExist:
+            # Create new mask according to image size
+            image_entry = self.table_images.get(self.table_images.id == image)
+            pil_image = Image.open(image_entry.filename)
+            im = np.zeros(pil_image.size, dtype=np.uint8)
+            return im
+    
+    def SetMask(self,mask, image, image_frame=0):
+        """
+        Add or overwrite the mask file for the image with the id `image` and the frame number `image_frame`
+    
+        Parameters
+        ----------
+        mask : array_like
+            mask image data.
+        image : int
+            image id.
+        image_frame : int, optional
+            image frame number (default=0).
+        """
+        try:
+            # Test if mask already exists in database
+            mask_entry = self.table_mask.get(self.table_mask.image == image, self.table_mask.image_frame == image_frame)
+            filename = mask_entry.filename
+        except peewee.DoesNotExist:
+            # Create new entry
+            mask_entry = self.table_mask(image=image, image_frame=image_frame)
+            # Get mask image name
+            image_entry = self.table_images.get(id=image)
+            if image_entry.frames > 1:
+                number = "_"+("%"+"%d" % np.ceil(np.log10(image_entry.frames))+"d") % image_frame
+            else:
+                number = ""
+            basename, ext = os.path.splitext(image_entry.filename)
+            directory, basename = os.path.split(basename)
+            current_maskname = os.path.join(directory, basename + "_" + ext[1:] + number + "_mask.png")
+            filename = current_maskname
+            # Save entry
+            mask_entry.filename = current_maskname
+            mask_entry.save()
+    
+        # Create image
+        pil_image = Image.fromarray(mask)
+        # Create color palette
+        lut = np.zeros(3 * 256, np.uint8)
+        for draw_type in self.table_maskTypes.select():
+            index = draw_type.index
+            lut[index * 3:(index + 1) * 3] = [int("0x"+draw_type.color[i:i+2], 16) for i in [1, 3, 5]]
+        pil_image.putpalette(lut)
+        # Save mask
+        pil_image.save(filename)
