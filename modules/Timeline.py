ca447fe4f2799fd8c1588888c9d36301bd130584
Randrian <Randrian@localhost>
2016-02-03 18:39:40 +0100

    * Timeline: added dynamic ticks

--- modules/Timeline.py
+++ modules/Timeline.py
@@ -30,6 +30,21 @@ def BoundBy(value, min, max):
         return max
     return value
 
+def roundTime(dt=None, roundTo=60):
+    """Round a datetime object to any time laps in seconds
+    dt : datetime.datetime object, default now.
+    roundTo : Closest number of seconds to round to, default 1 minute.
+    Author: Thierry Husson 2012 - Use it as you want but don't blame me.
+    """
+    if dt == None : dt = datetime.datetime.now()
+    seconds = (dt - dt.min).seconds
+    # // is a floor division, not a comment on following line:
+    rounding = (seconds+roundTo/2) // roundTo * roundTo
+    return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
+
+def roundValue(value, modulo, offset=0):
+    return int((value-offset) // modulo) * modulo + offset
+
 def DateDivision(x, y):
     return x.total_seconds()/y.total_seconds()
 
@@ -324,6 +339,13 @@ class RealTimeSlider(QGraphicsView):
         self.slider_line.setBrush(QBrush(gradient))
         #self.slider_line.mousePressEvent = self.SliderBarMousePressEvent
 
+        self.markerParent = QGraphicsPathItem(None, self.scene)
+        self.markerGroupParents = []
+        for i in range(20):
+            self.markerGroupParents.append(QGraphicsPathItem(self.markerParent))
+        #self.markerGroupParents[0].setVisible(False)
+
+
 
         path = QPainterPath()
         path.addRect(-2, -7, 5, 14)
@@ -338,46 +360,72 @@ class RealTimeSlider(QGraphicsView):
 
         self.tick_marker = {}
 
+        self.scale = 1
+        self.pan = 0
+
     def SliderBarMousePressEvent(self, event):
         self.setValue(self.PixelToValue(self.slider_line.mapToScene(event.pos()).x()))
         self.slider_position.signal.sliderReleased.emit()
 
-    def addTickMarker(self, pos, type=0, color=QColor("red"), height=12, text=""):
-        if type == 1:
-            color = QColor("green")
-            height = 8
+    def tick_time(self, tick_marker):
+        tick_marker.setAlpha(0.5)
+        tick_marker.timer.stop()
+        print("tick_time")
+
+    def addTickMarker(self, pos, type=-1, type_name="", color=QColor("red"), height=12, text=""):
         if pos in self.tick_marker and type in self.tick_marker[pos]:
             tick_marker = self.tick_marker[pos][type]
         else:
-            if type == 0:
-                tick_marker = QtGui.QGraphicsLineItem(0, -2, 0, -height, None, self.scene)
+            if type == -1:
+                tick_marker = QtGui.QGraphicsLineItem(0, -5, 0, -height, self.markerParent)
                 tick_marker.setZValue(-10)
             else:
-                tick_marker = QtGui.QGraphicsLineItem(0, -0.5, 0, -height, None, self.scene)
-                tick_marker.setZValue(1+type)
+                if type_name == "second":
+                    text = "%02d:%02d:%02d" % (pos.hour, pos.minute, pos.second)
+                elif type_name == "minute":
+                    text = "%02d:%02d" % (pos.hour, pos.minute)
+                elif type_name == "hour":
+                    text = "%02d:%02d" % (pos.hour, pos.minute)
+                elif type_name == "day":
+                    text = "%02d.%02d" % (pos.day, pos.month)
+                elif type_name == "month":
+                    text = "%02d.%02d" % (pos.day, pos.month)
+                elif type_name == "year":
+                    text = "%04d" % pos.year
+                tick_marker = QtGui.QGraphicsLineItem(0, -0.5, 0, -height, self.markerGroupParents[type])#self.scene)
+                tick_marker.setZValue(1)#+type)
         tick_marker.setPen(QPen(color))
+        if type == -1:
+            tick_marker.setPen(QPen(color, 3))
         #tick_marker.setBrush(QBrush(color))
         tick_marker.value = pos
         tick_marker.type = type
         tick_marker.height = height
 
+        #tick_marker.timer = QtCore.QTimer(self)
+        #tick_marker.timer.timeout.connect(lambda p=tick_marker: self.tick_time(p))
+        #tick_marker.timer.startTimer(10)
+
         tick_marker.setPos(self.ValueToPixel(pos), 0)
         if text != "":
+            self.font_parent = QtGui.QGraphicsPathItem(tick_marker)
+
             self.font = QtGui.QFont()
             self.font.setPointSize(8)
-            self.text = QtGui.QGraphicsSimpleTextItem(tick_marker)
+            self.text = QtGui.QGraphicsSimpleTextItem(self.font_parent)
             self.text.setFont(self.font)
             self.text.setText(text)
             offsetX = self.text.boundingRect().width()
             offsetY = self.text.boundingRect().height()
             self.text.setPos(-offsetX*0.5+1, 0)#+height*0.5+offsetY)
+            self.font_parent.setFlag(QtGui.QGraphicsItem.ItemIgnoresTransformations)
             tick_marker.text = self.text
         else:
             tick_marker.text = None
         if pos not in self.tick_marker:
             self.tick_marker[pos] = {}
         self.tick_marker[pos][type] = tick_marker
-        self.repaint()
+        #self.repaint()
 
     def removeTickMarker(self, pos, type=0):
         if pos in self.tick_marker and type in self.tick_marker[pos]:
@@ -409,10 +457,10 @@ class RealTimeSlider(QGraphicsView):
         return my_range[-1]
 
     def resizeEvent(self, event):
-        self.length = self.size().width()-20
+        self.length = self.size().width()#-20
         self.slider_line.setRect(0, -0.5, self.length, 1)
-        #self.slider_line_active.setRect(self.ValueToPixel(self.slider_start.value), 0, self.ValueToPixel(self.slider_end.value)-self.ValueToPixel(self.slider_start.value), 5)
-        #self.ensureVisible(self.slider_line)
+        self.setSceneRect(0, -10, self.size().width(), 20)
+        self.ensureVisible(self.slider_line)
         for pos, ticks in self.tick_marker.items():
             for type, tick in ticks.items():
                 tick.setPos(self.ValueToPixel(pos), 0)
@@ -458,16 +506,172 @@ class RealTimeSlider(QGraphicsView):
         for time in self.media_handler.timestamps:
             self.addTickMarker(time, color=QColor(128, 128, 128))
         start = self.media_handler.timestamps[0]
-        tick_time = datetime.datetime(year=start.year, month=start.month, day=start.day, hour=start.hour)#datetime.datetime.combine(self.media_handler.timestamps[0].date(), datetime.time())+datetime.timedelta(days=1)
+        tick_types = [["second", 5, 0],
+                      ["second", 10, 0],
+                      ["second", 0, 0],
+                      ["minute", 5, 0],
+                      ["minute", 10, 0],
+                      ["minute", 0, 0],
+                      ["hour", 3, 0],
+                      ["hour", 6, 0],
+                      ["hour", 12, 0],
+                      ["hour", 0, 0],
+                      ["day", 0, 1],
+                      ["month", 0, 1],
+                      ["year", 0, 1]]
+        tick_time = datetime.datetime(year=start.year, month=start.month, day=start.day, hour=start.hour, minute=start.minute)#datetime.datetime.combine(self.media_handler.timestamps[0].date(), datetime.time())+datetime.timedelta(days=1)
+        '''
         while tick_time < self.max_value:
-            if tick_time.day == 1 and tick_time.hour == 0:
+            type = 0
+            while 1:
+                tick_type = tick_types[type]
+                #print(tick_type, tick_type[1])
+                value = getattr(tick_time, tick_type[0])
+                #if tick_type[0] == "minute":
+                #    print(tick_type[0], value, tick_type[1], tick_type[2])
+                if tick_type[1]:
+                    value %= tick_type[1]
+                    #if tick_type[0] == "minute":
+                    #    print("Modul", value, tick_type[1], tick_type[0], getattr(tick_time, tick_type[0]))
+                if value != tick_type[2]:
+                    break
+                type += 1
+            #if type >= 2:
+            #    print(type)
+            self.addTickMarker(tick_time, color=QColor(0, 128, 255), height=15, type=type, type_name=tick_types[type][0])
+            """
+            if tick_time.minute == 0 and tick_time.day == 1 and tick_time.hour == 0:
                 self.addTickMarker(tick_time, color=QColor(0, 128, 255), text="%02d.%02d" % (tick_time.day, tick_time.month), height=15, type=12)
-            elif tick_time.hour == 0:
+            elif tick_time.minute == 0 and tick_time.hour == 0:
                 self.addTickMarker(tick_time, color=QColor(0, 0, 255), text="%02d" % tick_time.day, type=11)
+            elif tick_time.minute == 0:
+                self.addTickMarker(tick_time, color=QColor(50, 0, 128), text="%02dh" % tick_time.hour, height=8, type=10)
             else:
-                self.addTickMarker(tick_time, color=QColor(50, 0, 128), text="%02d" % tick_time.hour, height=8, type=10)
-            tick_time = tick_time+datetime.timedelta(hours=1)
+                self.addTickMarker(tick_time, color=QColor(50, 0, 128), text="%02d'" % tick_time.minute, height=8, type=10)
+            """
+            tick_time = tick_time+datetime.timedelta(**{tick_types[0][0]+"s":1})
+        '''
         print("End Timeline", timing.time()-t)
+
+    def updateTicks(self):
+
+        for pos, ticks in self.tick_marker.items():
+            for type, tick in ticks.items():
+                if type == -1:
+                    continue
+                if tick.scene():
+                    tick.scene().removeItem(tick)
+                del self.tick_marker[pos][type]
+                #del tick
+            if self.tick_marker[pos] == {}:
+                del self.tick_marker[pos]
+
+        # determine the smallest possible ticks
+        delta_min = self.PixelToValue(60)-self.min_value
+        type_deltas = [datetime.timedelta(seconds=1),
+                       #datetime.timedelta(seconds=2),
+                       datetime.timedelta(seconds=5),
+                       datetime.timedelta(seconds=10),
+                       #datetime.timedelta(seconds=15),
+                       datetime.timedelta(seconds=30),
+                       datetime.timedelta(minutes=1),
+                       #datetime.timedelta(minutes=2),
+                       datetime.timedelta(minutes=5),
+                       datetime.timedelta(minutes=10),
+                       #datetime.timedelta(minutes=15),
+                       datetime.timedelta(minutes=30),
+                       datetime.timedelta(hours=1),
+                       #datetime.timedelta(hours=2),
+                       datetime.timedelta(hours=3),
+                       datetime.timedelta(hours=6),
+                       datetime.timedelta(hours=12),
+                       datetime.timedelta(days=1),
+                       #datetime.timedelta(days=2),
+                       #datetime.timedelta(days=5),
+                       #datetime.timedelta(days=10),
+                       #datetime.timedelta(days=15),
+                       datetime.timedelta(days=30),
+                       #datetime.timedelta(days=30*2),
+                       datetime.timedelta(days=30*3),
+                       datetime.timedelta(days=30*6),
+                       datetime.timedelta(days=356),
+                       datetime.timedelta(days=356*5),
+                       datetime.timedelta(days=356*10),
+                       datetime.timedelta(days=356*50),
+                       datetime.timedelta(days=356*100),
+                       ]
+        type_delta = type_deltas[0]
+        type_delta_minor = type_deltas[0]
+        for type_delta_test in type_deltas:
+            type_delta = type_delta_test
+            if type_delta_test > delta_min:
+                break
+            type_delta_minor = type_delta_test
+        #print(self.ValueToPixel(type_delta+self.min_value), self.ValueToPixel(delta_min+self.min_value))
+
+        tick_types = [["second", 0, 0],
+                      ["minute", 0, 0],
+                      ["hour", 0, 0],
+                      ["day", 0, 1],
+                      ["month", 0, 1],
+                      ["year", 0, 1]]
+        # round to the nearest tick
+        years = 0
+        months = 0
+        if type_delta >= datetime.timedelta(days=356):
+            # round to years
+            years = int(type_delta.days/356)
+            tick_time = datetime.datetime(roundValue(self.min_value.year, years), 1, 1)
+        elif type_delta >= datetime.timedelta(days=30):
+            # round to months
+            months = int(type_delta.days/30)
+            print("############# rounding to months", roundValue(self.min_value.month, months, 1), months)
+            tick_time = datetime.datetime(self.min_value.year, roundValue(self.min_value.month, months, 1), 1)
+        else:
+            tick_time = roundTime(self.min_value, type_delta.total_seconds())
+
+        type_delta_major = type_delta
+        type_delta = type_delta_minor
+        print("Major Minor", type_delta_major, type_delta_minor)
+
+        count = 0
+        self.tick_start = tick_time
+        while tick_time < self.max_value:
+            type = 0
+            while 1:
+                tick_type = tick_types[type]
+                #print(tick_type, tick_type[1])
+                value = getattr(tick_time, tick_type[0])
+                #if tick_type[0] == "minute":
+                #    print(tick_type[0], value, tick_type[1], tick_type[2])
+                if tick_type[1]:
+                    value %= tick_type[1]
+                    #if tick_type[0] == "minute":
+                    #    print("Modul", value, tick_type[1], tick_type[0], getattr(tick_time, tick_type[0]))
+                if value != tick_type[2]:
+                    break
+                type += 1
+            #if type >= 2:
+            #    print(type)
+            #print("Major?", (tick_time-self.tick_start).total_seconds() % type_delta_major.total_seconds(), type_delta_major.total_seconds())
+            if (tick_time-self.tick_start).total_seconds() % type_delta_major.total_seconds() == 0:
+                self.addTickMarker(tick_time, color=QColor(0, 0, 0), height=15, type=type, type_name=tick_types[type][0])
+            else:
+                self.addTickMarker(tick_time, color=QColor(0, 0, 0), height=10, type=type, type_name="")
+            if years:
+                tick_time = datetime.datetime(tick_time.year+years, tick_time.month, 1)
+            elif months:
+                if tick_time.month+months > 12:
+                    tick_time = datetime.datetime(tick_time.year+1, 1, 1)
+                else:
+                    tick_time = datetime.datetime(tick_time.year, tick_time.month+months, 1)
+            else:
+                tick_time = tick_time+type_delta
+            count += 1
+        print(type_delta, count)
+        self.repaint()
+        #type_jumps = [{"seconds": 1}, {"seconds": 5}, {"seconds": 10}, {"minutes": 1}, {"minutes": 5}, {"minutes": 10}, {"hours": 1}, {"hours": 3}, {"hours": 6}, {"hours": 12}, {"days": 1}, {"days": 30}, {"days": 356}]
+
 
     def mousePressEvent(self, event):
         if event.button() == 2:
@@ -479,15 +683,18 @@ class RealTimeSlider(QGraphicsView):
         if self.scene_panning:
             new_pos = PosToArray(self.mapToScene(event.pos()))
             delta = (new_pos-self.last_pos)[0]
+            self.last_pos = new_pos
             print(delta)
             delta = self.PixelToValue(delta)-self.min_value
             print(delta)
+            #self.markerParent.translate(delta/self.scale, 0)
             self.min_value -= delta
             self.max_value -= delta
             #self.translater.setTransform(QtGui.QTransform(1, 0, 0, 1, *delta/self.scaler.transform().m11()), combine=True)
             self.last_pos = new_pos
             self.fitted = 0
             self.slider_position.setValueRange(self.min_value, self.max_value)
+            self.updateTicks()
             self.resizeEvent(None)
         super(RealTimeSlider, self).mouseMoveEvent(event)
 
@@ -497,6 +704,37 @@ class RealTimeSlider(QGraphicsView):
             self.scene_panning = False
         super(RealTimeSlider, self).mouseReleaseEvent(event)
 
+
+    def wheelEvent(self, event):
+        event.ignore()
+        super(RealTimeSlider, self).wheelEvent(event)
+        if event.isAccepted():
+            return
+
+        if 0:#qt_version == '5':
+            angle = event.angleDelta().y()
+        else:
+            angle = event.delta()
+        if angle > 0:
+            self.scale *= 1.1
+            self.markerParent.scale(1.1, 1)
+            #self.scaleOrigin(1.1, event.pos())
+        else:
+            self.scale *= 0.9
+            self.markerParent.scale(0.9, 1)
+            #self.scaleOrigin(0.9, event.pos())
+        event.accept()
+
+        self.updateTicks()
+        """
+        tick_types = ["second", "5 seconds", "10 seconds", "minute", "5 minutes", "10 minutes", "hour", "day", "month", "year"]
+        type_jumps = [{"seconds": 1}, {"seconds": 5}, {"seconds": 10}, {"minutes": 1}, {"minutes": 5}, {"minutes": 10}, {"hours": 1}, {"hours": 3}, {"hours": 6}, {"hours": 12}, {"days": 1}, {"days": 30}, {"days": 356}]
+        tick_type_defaults = [0, 0, 1, 1, 1]
+        for index, type_jump in enumerate(type_jumps):
+            type_pixel = self.ValueToPixel(datetime.timedelta(**type_jump)+self.min_value)*self.scale
+            self.markerGroupParents[index].setVisible(type_pixel > 40)
+            #print(tick_types[index], type_pixel)
+        """
 
     def wheelEvent(self, event):
         event.ignore()
@@ -526,6 +764,9 @@ class RealTimeSlider(QGraphicsView):
             self.min_value += datetime.timedelta(seconds=zoom_delta.total_seconds()*percent)
             self.max_value -= datetime.timedelta(seconds=zoom_delta.total_seconds()*(1-percent))
         self.slider_position.setValueRange(self.min_value, self.max_value)
+
+        self.updateTicks()
+        """
         hour_pixel = self.ValueToPixel(datetime.timedelta(hours=1)+self.min_value)
         day_pixel = self.ValueToPixel(datetime.timedelta(days=1)+self.min_value)
 #        if hour_pixel < 15:
@@ -542,6 +783,8 @@ class RealTimeSlider(QGraphicsView):
                     tick.pen().setWidth(50)#+(hour_pixel > 15)*5)
                     tick.setVisible(day_pixel > 15)
                     tick.text.setVisible(day_pixel > 20)
+        self.resizeEvent(None)
+        """
         self.resizeEvent(None)
         event.accept()
 
