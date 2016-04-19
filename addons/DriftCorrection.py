228ea75824b29d2b23f202ded67959bbe8ba29a7
Fox <Fox@localhost>
2016-04-19 17:24:52 +0200

    * DrifftCorrection: Updated drift correction for newest version

--- addons/DriftCorrection.py
+++ addons/DriftCorrection.py
@@ -4,13 +4,13 @@ import numpy as np
 from scipy.ndimage.measurements import center_of_mass
 import peewee
 import sys
+import argparse
 
-from clickpoints.SendCommands import HasTerminateSignal, CatchTerminateSignal, GetImage, ReloadTypes
-from clickpoints.MarkerLoad import DataFile
+import clickpoints
 
 # Connect to database
-df = DataFile()
-CatchTerminateSignal()
+df = clickpoints.DataFile()
+com = clickpoints.Commands(catch_terminate_signal=True)
 
 # Define parameters
 compare_to_first = False
@@ -20,7 +20,7 @@ border_y = 20
 # Check if the marker type is present
 if not df.GetType("drift_rect"):
     df.AddType("drift_rect", [0, 255, 255], df.TYPE_Rect)
-    ReloadTypes()
+    com.ReloadTypes()
 
 # try to load marker
 rect = df.GetMarker(type_name="drift_rect")
@@ -36,9 +36,18 @@ roi_width2 = np.abs(np.diff(x))
 roi_y2 = np.min(y)
 roi_height2 = np.abs(np.diff(y))
 
+
 # get start data
-start_frame = int(sys.argv[2])
-img, image_id, image_frame = GetImage(start_frame)
+# get start frame from command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--start_frame", type=int, dest='start_frame', help='specify at which frame to start')
+args, unknown = parser.parse_known_args()
+if args.start_frame:
+    start_frame = args.start_frame
+else:
+    start_frame = 0
+
+img, image_id = com.GetImage(start_frame)
 template = img[roi_y2-border_y:roi_y2+roi_height2+border_y, roi_x2-border_x:roi_x2+roi_width2+border_x]
 
 # start iteration
@@ -47,7 +56,7 @@ i = start_frame
 while True:
     # get next image if there is one
     i += 1
-    img, image_id, image_frame = GetImage(i)
+    img, image_id = com.GetImage(i)
     if img is None:
         break
 
@@ -85,15 +94,15 @@ while True:
 
     # save the offset to the database
     try:
-        offset = df.table_offsets.get(image=image_id, image_frame=image_frame)
+        offset = df.table_offsets.get(image=image_id)
         offset.x = shift[1]
         offset.y = shift[0]
         offset.save()
     except peewee.DoesNotExist:
-        df.table_offsets(image=image_id, image_frame=image_frame, x=shift[1], y=shift[0]).save()
+        df.table_offsets(image=image_id, x=shift[1], y=shift[0]).save()
     print("Drift Correction Frame", i, shift)
 
     # Check if ClickPoints wants to terminate us
-    if HasTerminateSignal():
-        print("Terminating Drift Correction")
+    if com.HasTerminateSignal():
+        print("Cancelle Stabilization")
         sys.exit(0)
