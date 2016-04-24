79c3a410f2561b71a23fb6eead139a09d1c55098
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2016-04-24 17:43:34 +0200

    * Tack.py: cleaned and simplified script

--- addons/Track.py
+++ addons/Track.py
@@ -1,9 +1,6 @@
 from __future__ import print_function, division
 import sys
 import numpy as np
-import os
-import time
-import argparse
 
 # define tracking parameter
 import cv2
@@ -12,48 +9,49 @@ lk_params = dict(winSize=(8, 8), maxLevel=0, criteria=(cv2.TERM_CRITERIA_EPS | c
 # connect to ClickPoints database and the running program instance
 # database filename and port for communication are supplied as command line argument when started from ClickPoints
 import clickpoints
-df = clickpoints.DataFile()
-com = clickpoints.Commands(catch_terminate_signal=True)
+start_frame, database, port = clickpoints.GetCommandLineArgs()
+db = clickpoints.DataFile(database)
+com = clickpoints.Commands(port, catch_terminate_signal=True)
 
-# get start frame from command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--start_frame", type=int, dest='start_frame', help='specify at which frame to start')
-args, unknown = parser.parse_known_args()
-if args.start_frame:
-    start_frame = args.start_frame
-else:
-    start_frame = 0
+# get the images
+images = db.GetImages(start_frame=start_frame)
 
-image_data_current, image_id_current = com.GetImage(start_frame)
-points = df.GetMarker(image=image_id_current, processed=0)
+# retrieve first image
+image_last = images[0]
+
+# get points and corresponding tracks
+points = db.GetMarker(image=image_last.id, processed=0)
 p0 = np.array([[point.x, point.y] for point in points if point.track_id]).astype(np.float32)
 tracking_ids = [point.track_id for point in points if point.track_id]
 types = [point.type_id for point in points if point.track_id]
 
+# if no tracks are supplied, stop
 if len(tracking_ids) == 0:
     print("Nothing to track")
     sys.exit(-1)
 
-frame = start_frame
-while True:
-    image_data_next, image_id_next = com.GetImage(frame+1)
-    if image_data_next is None:
-        print("Reached end")
-        break
+# start iterating over all images
+for image in images[1:]:
+    print("Tracking frame number %d, %d tracks" % (image.sort_index, len(tracking_ids)))
 
-    print("Tracking frame number %d, %d tracks" % (frame, len(tracking_ids)))
-    p1, st, err = cv2.calcOpticalFlowPyrLK(image_data_current, image_data_next, p0, None, **lk_params)
+    # calculate next positions
+    p1, st, err = cv2.calcOpticalFlowPyrLK(image_last.data8, image.data8, p0, None, **lk_params)
 
-    df.SetMarker(image=image_id_next, x=p1[:, 0], y=p1[:, 1], processed=0, type=types, track=tracking_ids)
-    df.SetMarker(image=image_id_current, processed=1, type=types, track=tracking_ids)
-    com.ReloadMarker(frame+1)
-    com.JumpToFrameWait(frame+1)
+    # set the new positions
+    db.SetMarker(image=image.id, x=p1[:, 0], y=p1[:, 1], processed=0, type=types, track=tracking_ids)
 
+    # mark the marker in the last frame as processed
+    db.SetMarker(image=image_last.id, processed=1, type=types, track=tracking_ids)
+
+    # update ClickPoints
+    com.ReloadMarker(image.sort_index)
+    com.JumpToFrameWait(image.sort_index)
+
+    # store positions and image
     p0 = p1
-    image_id_current = image_id_next
-    image_data_current = image_data_next
-    frame += 1
+    image_last = image
 
+    # check if we should terminate
     if com.HasTerminateSignal():
         print("Cancelled Tracking")
         sys.exit(0)
