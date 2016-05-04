781216384522af36ef08b5b12d665daefa648816
Randrian <Randrian@localhost>
2016-05-04 17:47:06 +0200

    ! Includes: HTMLColorToRGB, TextButton were missing in __init__.py

--- includes/__init__.py
+++ includes/__init__.py
@@ -2,7 +2,7 @@ import sys, os
 
 sys.path.append(os.path.dirname(__file__))
 from ConfigLoad import LoadConfig, ExceptionPathDoesntExist
-from Tools import HelpText, BroadCastEvent, BroadCastEvent2, SetBroadCastModules, rotate_list
+from Tools import HelpText, BroadCastEvent, BroadCastEvent2, SetBroadCastModules, rotate_list, HTMLColorToRGB, TextButton
 from BigImageDisplay import BigImageDisplay
 from Database import DataFile
 from MemMap import MemMap
