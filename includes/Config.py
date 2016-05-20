fee8d4488484cff135bc94457f26b2de63b5c2a8
Fox <57496278+foxsr@users.noreply.github.com>
2016-05-20 13:42:08 +0200

    + Config.py added to includes, added defaults dictionary to constructor

---
+++ includes/Config.py
@@ -0,0 +1,28 @@
+import ConfigParser
+import json
+
+class ConfigAccessHelper:
+    def __init__(self, parent, section):
+        self.parent = parent
+        self.section = section
+
+    def __getattr__(self, key):
+        try:
+            data = self.parent.get(self.section, key)
+        except ConfigParser.NoOptionError:
+            return ""
+        try:
+            return json.loads(data)
+        except ValueError:
+            return data
+
+class Config(ConfigParser.ConfigParser):
+    def __init__(self, filename=None,defaults=None, *args, **kwargs):
+        ConfigParser.ConfigParser.__init__(self, defaults=defaults, *args, **kwargs)
+        if filename is not None:
+            self.read(filename)
+
+    def __getattr__(self, key):
+        if key in self.sections():
+            return ConfigAccessHelper(self, key)
+        return ConfigParser.ConfigParser.__getattr__(self, key)
