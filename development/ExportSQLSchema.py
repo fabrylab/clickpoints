273869019193cd04fc1466f3dbf009eb19dc1627
Randrian <Randrian@localhost>
2016-07-04 16:22:09 +0200

    * ExportSQLSchema.py more generic and CREATE INDEX at the end, commands are terminated with a ";"

--- development/ExportSQLSchema.py
+++ development/ExportSQLSchema.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
-import peewee
+import clickpoints
+import os
 
 def dict_factory(cursor, row):
     d = {}
@@ -7,9 +8,12 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-db = peewee.SqliteDatabase(r"D:\Repositories\ClickPointsExamples\PlantRoot\Test_MaskHandler_Test_MaskHandler_test_brushSizeMask.db_mask.png\test.cdb")
-db.get_conn().row_factory = dict_factory
+db = clickpoints.DataFile("tmp.cdb", "w")
+db.db.get_conn().row_factory = dict_factory
 
 with open("schema.sql", "w") as fp:
-    for row in db.execute_sql("SELECT * FROM sqlite_master ORDER BY tbl_name;").fetchall():
-        fp.write(row["sql"]+"\n")
+    for row in db.db.execute_sql("SELECT * FROM sqlite_master ORDER BY type DESC, name").fetchall():
+        fp.write(row["sql"]+";\n")
+db.db.close()
+
+os.remove("tmp.cdb")
