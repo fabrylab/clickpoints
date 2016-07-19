caa5b01338d310c42e0d3afbbf5320733cbd7d12
Fox <57496278+foxsr@users.noreply.github.com>
2016-07-19 18:20:40 +0200

    + Addons: ExportMarkerCountToXLS - demo for db to xls export

---
+++ addons/ExportMarkerCountToXLS.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+import xlwt
+
+# connect to ClickPoints database
+# database filename is supplied as command line argument when started from ClickPoints
+import clickpoints
+start_frame, database, port = clickpoints.GetCommandLineArgs()
+db = clickpoints.DataFile(database)
+com = clickpoints.Commands(port, catch_terminate_signal=True)
+
+# prepare write to excel
+wb_name = database.replace('.cdb','.xls')
+wb = xlwt.Workbook()
+wb_sheet = wb.add_sheet('Eval1')
+
+# get types and images
+q_types =  db.GetTypes()
+q_images = db.GetImages()
+
+# write xls header
+wb_sheet.write(0,0, "sort_idx")
+wb_sheet.write(0,1, "filename")
+for idx,type in enumerate(q_types):
+    wb_sheet.write(0,idx+2, type.name + '_count')
+header_offset = 1
+
+# write marker counts per image
+for ridx,image in enumerate(q_images):
+    ridx = ridx + header_offset
+    wb_sheet.write(ridx,0,image.sort_index)
+    wb_sheet.write(ridx,1,image.filename)
+
+    # extract type information
+    for idx,type in enumerate(q_types):
+        q_marker = db.GetMarker(type=type,image=image)
+        wb_sheet.write(ridx,idx+2,q_marker.count())
+
+print("Writing to %s" % wb_name)
+wb.save(wb_name)
+print("DONE")
