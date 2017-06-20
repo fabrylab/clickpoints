6278c5d73664fe9150ce84a209d53c04c0ee3296
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2017-06-20 09:30:40 +0200

    + Addon: add-ons can now overload their behavior when the button is pressed

--- package/clickpoints/Addon.py
+++ package/clickpoints/Addon.py
@@ -58,8 +58,9 @@ class Command:
     script_launcher = None
     stop = False
 
-    def __init__(self, script_launcher=None):
+    def __init__(self, script_launcher=None, script=None):
         self.script_launcher = script_launcher
+        self.script = script
         if self.script_launcher is not None:
             self.window = self.script_launcher.window
 
@@ -251,62 +252,133 @@ class Command:
             return self.window.view.hud_lowerRight
         raise NameError("%s no valid location name" % location)
 
+    STATUS_Idle = 0
+    STATUS_Active = 1
+    STATUS_Running = 2
 
+    def setStatus(self, status=0):
+        """
+        Set the button state for the add-on.
+        
+        Parameters
+        ----------
+        status : int
+            the button can have three states, STATUS_Idle for an non active button, STATUS_Active for an active button and
+            STATUS_Running for an active button with an hourglass symbol.
+        """
+
+        self.script_launcher.setStatus(self.script.addon_name, status)
 
 
 class Addon(QtWidgets.QWidget):
     _run_thread = None
+    _change_status = QtCore.Signal(int)
 
     def __init__(self, database, command=None, name="", database_class=None, icon=None):
+        # initialize the Widget base class
         QtWidgets.QWidget.__init__(self)
+
+        # overload two matplotlib functions to help use them from the run function from a different thread
         plt.show = show
         plt.figure = figure
-        self.cp = Command(command)
+
+        # initialize the command class to communicate with ClickPoints
+        self.cp = Command(command, self)
+
+        # get the database instance, either it is already a database object or a filename
         if isinstance(database, str):
+            # if we have a filename, open the file with the provided database class type or the default type
             if database_class:
                 self.db = database_class(database)
             else:
                 self.db = clickpoints.DataFile(database)
         else:
+            # store the database object
             self.db = database
+            # if the object should have a different class, convert it
             if database_class is not None:
+                # store some pointers to the options
                 _options = self.db._options
                 _options_by_key = self.db._options_by_key
+                # initiate a new database class instance with the new class type
                 self.db = database_class(self.db.db.database)
+                # and put the options pointers back in place
                 self.db._options = _options
                 self.db._options_by_key = _options_by_key
+
+        # remember the add-on name
         self.addon_name = name
+        # create an option category for the add-on
         self._options_category = "Addon - "+name
         self.db._last_category = self._options_category
 
-        self.setWindowIcon(icon)
+        # set the icon for the add-on, if provided
+        if icon is not None:
+            self.setWindowIcon(icon)
+
+        # wrap the run function, so that it automatically updates the current state of the add-on (for the button state in ClickPoints)
+        function = self.run
+        def run_wrapper(*args, **kwargs):
+            self.run_started()
+            try:
+                function(*args, **kwargs)
+            finally:
+                self.run_stopped()
+        self.run = run_wrapper
+
+        # connect the status changed signal (to be able to change the status from another thread)
+        self._change_status.connect(self.cp.setStatus)
 
     def _warpOptionKey(self, key):
+        # wrap an option keyword with the add-on's name
         return "addon_" + self.addon_name.replace(" ", "") + "_" + key
 
     def addOption(self, key="", **kwargs):
+        # add an option for the add-on
         self.db._AddOption(key=self._warpOptionKey(key), **kwargs)
 
     def getOption(self, key):
+        # get the option value for the add-on
         return self.db.getOption(key=self._warpOptionKey(key))
 
     def getOptions(self):
+        # get a list of all options as options objects
         return self.db._options_by_key[self._options_category]
 
     def setOption(self, key, value):
+        # set the value of an option
         return self.db.setOption(key=self._warpOptionKey(key), value=value)
 
+    def buttonPressedEvent(self):
+        # callback that gets called when the user clicks the button of the add-on in ClickPoints
+        self.run_threaded()
+
     def terminate(self):
+        # when the add-on wants to tell it's run thread to terminate
         self.cp.stop = True
         self._run_thread.join(1)
         self._run_thread = None
 
     def is_running(self):
+        # check if the run thread is running
         if self._run_thread and self._run_thread.isAlive():
             return True
         return False
 
+    def run_started(self):
+        # called before run, by the run_wrapper to update the state of the add-on
+        self.cp.stop = False
+        self._change_status.emit(self.cp.STATUS_Running)
+
+    def run_stopped(self):
+        # called after run, by the run_wrapper to update the state of the add-on
+        if self.isVisible():
+            self._change_status.emit(self.cp.STATUS_Active)
+        else:
+            self._change_status.emit(self.cp.STATUS_Idle)
+
     def run_threaded(self, start_frame=None):
+        # start the run function in another thread, or stop it, if it is already running
         if self.is_running():
             self.terminate()
         else:
@@ -317,7 +389,19 @@ class Addon(QtWidgets.QWidget):
             self._run_thread.start()
 
     def run(self, start_frame=0):
+        # here the add-on can implement it's run routine
         pass
 
     def delete(self):
+        # callback that gets called if ClickPoints wants to remove the add-on (also used before reloading the add-on)
         pass
+
+    def showEvent(self, event):
+        # when the add-on displays it's GUI, we change the status of the add-on
+        if not self.is_running():
+            self.cp.setStatus(self.cp.STATUS_Active)
+
+    def closeEvent(self, event):
+        # when the add-on hides it's GUI, we change the status of the add-on
+        if not self.is_running():
+            self.cp.setStatus(self.cp.STATUS_Idle)
