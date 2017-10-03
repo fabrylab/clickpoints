30cbc6bf4ea73c490d99f04f15148bc6c02c11dc
Randrian <Randrian@localhost>
2017-10-03 20:54:15 +0200

    + added "clickpoints ffmpeg" to install ffmpeg directly from the installer

--- clickpoints/launch.py
+++ clickpoints/launch.py
@@ -9,6 +9,32 @@ def main():
         elif sys.argv[1] == "unregister" or sys.argv[1] == "uninstall":
             from .includes.RegisterRegistry import install
             return install("uninstall")
+        elif sys.argv[1] == "-v" or sys.argv[1] == "--version":
+            import clickpoints
+            print(clickpoints.__version__)
+            return
+        elif sys.argv[1] == "ffmpeg":
+            import imageio
+            import glob
+            import os
+            # check for ffmpeg
+            try:
+                # check if imageio already has an exe file
+                imageio.plugins.ffmpeg.get_exe()
+                print("ffmpeg found from imageio")
+            except imageio.core.fetching.NeedDownloadError:
+                # try to find an ffmpeg.exe in the ClickPoints folder
+                files = glob.glob(os.path.join(os.path.dirname(__file__), "..", "ffmpeg*.exe"))
+                files.extend(glob.glob(os.path.join(os.path.dirname(__file__), "..", "external", "ffmpeg*.exe")))
+                # if an ffmpeg exe has been found, set the environmental variable accordingly
+                if len(files):
+                    print("ffmpeg found", files[0])
+                    os.environ['IMAGEIO_FFMPEG_EXE'] = files[0]
+                # if not, try to download it
+                else:
+                    print("try to download ffmpeg")
+                    imageio.plugins.ffmpeg.download()
+            return
 
     from clickpoints import print_status
     # print
