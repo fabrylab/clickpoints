ef1617e54ba0864b2df24c32c657072e1b104492
Randrian <Randrian@localhost>
2017-10-05 10:16:49 +0200

    * install_requirements_with_conda.py: added conda-forge channel

--- install_requirements_with_conda.py
+++ install_requirements_with_conda.py
@@ -1,12 +1,13 @@
 import os
 
+os.system("conda config --add channels conda-forge")
 with open("meta.yaml", 'r') as fp:
     active = False
     for line in fp:
         line = line.strip()
         if line == "run:":
             active = True
-        elif active == True:
+        elif active is True:
             if line.startswith("-"):
                 package = line[2:]
                 if package != "python":
