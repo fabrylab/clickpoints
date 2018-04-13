ba0985f102ed651cfac9b16b7bb96bcb096de837
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2018-04-13 12:40:47 +0200

    * Add-on GrabPlotData: added input and display for x and y limits

--- clickpoints/addons/GrabPlotData/GrabPlotData.py
+++ clickpoints/addons/GrabPlotData/GrabPlotData.py
@@ -22,6 +22,7 @@
 from __future__ import division, print_function
 import os, sys
 import clickpoints
+from qtpy import QtCore, QtGui, QtWidgets
 
 
 def Remap(value, minmax1, minmax2):
@@ -34,6 +35,46 @@ def Remap(value, minmax1, minmax2):
     return percentage*length2 + minmax2[0]
 
 
+class QRangeInput(QtWidgets.QWidget):
+    valueChanged = QtCore.Signal(float, float)
+    no_signal = False
+
+    def __init__(self, layout, name, range):
+        QtWidgets.QWidget.__init__(self)
+        self.layout = QtWidgets.QHBoxLayout(self)
+
+        self.label = QtWidgets.QLabel(name)
+        self.layout.addWidget(self.label)
+
+        self.input1 = QtWidgets.QDoubleSpinBox()
+        self.input1.setValue(range[0])
+        self.input1.valueChanged.connect(self.valueChangedEvent)
+        self.input1.setRange(-999999, 999999)
+        self.layout.addWidget(self.input1)
+
+        self.input2 = QtWidgets.QDoubleSpinBox()
+        self.input2.setValue(range[1])
+        self.input2.valueChanged.connect(self.valueChangedEvent)
+        self.input2.setRange(-999999, 999999)
+        self.layout.addWidget(self.input2)
+
+        if layout is not None:
+            layout.addWidget(self)
+
+    def valueChangedEvent(self):
+        if self.no_signal is False:
+            self.valueChanged.emit(*self.range())
+
+    def range(self):
+        return (self.input1.value(), self.input2.value())
+
+    def setRange(self, lower, upper):
+        self.no_signal = True
+        self.input1.setValue(lower)
+        self.input2.setValue(upper)
+        self.no_signal = False
+
+
 class Addon(clickpoints.Addon):
     def __init__(self, *args, **kwargs):
         clickpoints.Addon.__init__(self, *args, **kwargs)
@@ -41,10 +82,10 @@ class Addon(clickpoints.Addon):
         # Check if the marker types are present
         reload_types = False
         if not self.db.getMarkerType("x_axis"):
-            self.db.setMarkerType("x_axis", [0, 200, 0], self.db.TYPE_Line, text="0, 1")
+            self.db.setMarkerType("x_axis", [0, 200, 0], self.db.TYPE_Line)
             reload_types = True
         if not self.db.getMarkerType("y_axis"):
-            self.db.setMarkerType("y_axis", [200, 200, 0], self.db.TYPE_Line, text="0, 1")
+            self.db.setMarkerType("y_axis", [200, 200, 0], self.db.TYPE_Line)
             reload_types = True
         if not self.db.getMarkerType("data"):
             self.db.setMarkerType("data", [200, 0, 0], self.db.TYPE_Normal)
@@ -52,9 +93,95 @@ class Addon(clickpoints.Addon):
         if reload_types:
             self.cp.reloadTypes()
 
+        # set the title and layout
+        self.setWindowTitle("Grab Plot Data - ClickPoints")
+        self.layout = QtWidgets.QVBoxLayout(self)
+
+        self.inputx = QRangeInput(self.layout, "x range", (0, 1))
+        self.inputx.valueChanged.connect(self.updateMarkers)
+        self.inputy = QRangeInput(self.layout, "y range", (0, 1))
+        self.inputy.valueChanged.connect(self.updateMarkers)
+
+        self.overlays = {}
+
+        self.image = QtWidgets.QGraphicsPixmapItem(self.cp.window.view.origin)
+        self.image.setZValue(5)
+
+        self.button_run = QtWidgets.QPushButton("Export")
+        self.button_run.clicked.connect(self.run)
+        self.layout.addWidget(self.button_run)
+
+    def updateMarker(self, marker):
+        if marker.type.name != "x_axis" and marker.type.name != "y_axis":
+            return
+        if marker.id not in self.overlays:
+            overlay = QtWidgets.QGraphicsPixmapItem(self.image)
+            overlay.text = []
+            overlay.text_parents = []
+            overlay.setZValue(10)
+            for i in range(2):
+                text_parent = QtWidgets.QGraphicsPixmapItem(overlay)
+                text = QtWidgets.QGraphicsSimpleTextItem(text_parent)
+                text_parent.setFlag(QtWidgets.QGraphicsItem.ItemIgnoresTransformations)
+                text.setZValue(10)
+                overlay.text.append(text)
+                overlay.text_parents.append(text_parent)
+                text.setBrush(QtGui.QBrush(QtGui.QColor(255, 0, 0, 255)))
+            self.overlays[marker.id] = overlay
+        else:
+            overlay = self.overlays[marker.id]
+        index = marker.type.name.startswith("y")
+
+        if index == 0:
+            values = self.inputx.range()
+        else:
+            values = self.inputy.range()
+
+        positions = [(marker.x1, marker.y1), (marker.x2, marker.y2)]
+        for i in range(2):
+            overlay.text[i].setText(str(values[i]))
+            rect = overlay.text[0].sceneBoundingRect()
+            if index == 1:
+                overlay.text_parents[i].setPos(positions[i][0], positions[i][1])
+                overlay.text[i].setPos(- rect.width() - 5, - rect.height() / 2)
+            else:
+                overlay.text_parents[i].setPos(positions[i][0], positions[i][1])
+                overlay.text[i].setPos(- rect.width()/2, 5)
+
+    def updateMarkers(self):
+        for id in self.overlays:
+            entry = self.db.getLine(id=id)
+            if entry is not None:
+                self.updateMarker(entry)
+
+    def deleteOverlay(self, marker):
+        if marker.type.name != "x_axis" and marker.type.name != "y_axis":
+            return
+        if marker.id in self.overlays:
+            overlay = self.overlays[marker.id]
+            overlay.scene().removeItem(overlay)
+            del self.overlays[marker.id]
+
+    def markerMoveEvent(self, marker):
+        self.updateMarker(marker)
+
+    def markerAddEvent(self, entry):
+        self.updateMarker(entry)
+
+    def markerRemoveEvent(self, entry):
+        self.deleteOverlay(entry)
+
+    def delete(self):
+        ids = [id for id in self.overlays.keys()]
+        for id in ids:
+            overlay = self.overlays[id]
+            overlay.scene().removeItem(overlay)
+            del self.overlays[id]
+        self.image.scene.removeItem(self.image)
+
     def run(self, start_frame=0):
         # get the current image
-        image = next(self.db.getImageIterator(start_frame))
+        image = self.db.getImage(self.cp.getCurrentFrame())
 
         # try to load axis
         x_axis = self.db.getLines(image=image, type="x_axis")
@@ -66,8 +193,8 @@ class Addon(clickpoints.Addon):
         y_axis = y_axis[0]
 
         # create remap functions for x and y axis
-        remap_x = lambda x: Remap(x, [min(x_axis.x1, x_axis.x2), max(x_axis.x1, x_axis.x2)], [float(x) for x in x_axis.text.split(",")])
-        remap_y = lambda y: Remap(y, [min(y_axis.y1, y_axis.y2), max(y_axis.y1, y_axis.y2)], [float(y) for y in y_axis.text.split(",")])
+        remap_x = lambda x: Remap(x, [x_axis.x1, x_axis.x2], self.inputx.range())
+        remap_y = lambda y: Remap(y, [y_axis.y1, y_axis.y2], self.inputy.range())
 
         # get all markers
         markers = self.db.getMarkers(image=image, type="data")
@@ -80,3 +207,6 @@ class Addon(clickpoints.Addon):
                 fp.write("%f %f\n" % (remap_x(data.x), remap_y(data.y)))
         # print success
         print("%d datepoints written to file \"%s\"" % (markers.count(), filename))
+
+    def buttonPressedEvent(self):
+        self.show()
