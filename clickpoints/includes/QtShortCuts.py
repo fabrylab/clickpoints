45b388461997820ffd7b6f88138ec1775930a05d
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2018-07-05 21:51:20 +0200

    * QtShortCuts: added widget overloads to simplify inputs for numbers, strings, choices, bools or colors

--- clickpoints/includes/QtShortCuts.py
+++ clickpoints/includes/QtShortCuts.py
@@ -19,11 +19,235 @@
 # You should have received a copy of the GNU General Public License
 # along with ClickPoints. If not, see <http://www.gnu.org/licenses/>
 
-from qtpy import QtGui, QtWidgets
-import os
 import colorsys
+import os
+
+import matplotlib as mpl
 import numpy as np
+from qtpy import QtCore, QtGui, QtWidgets
 from . import HTMLColorToRGB
+
+
+class QInput(QtWidgets.QWidget):
+    # the signal when the user has changed the value
+    valueChanged = QtCore.Signal('PyQt_PyObject')
+
+    no_signal = False
+
+    last_emited_value = 0
+
+    def __init__(self, layout=None, name=None):
+        # initialize the super widget
+        super(QInput, self).__init__()
+
+        # initialize the layout of this widget
+        QtWidgets.QHBoxLayout(self)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+
+        # add me to a parent layout
+        if layout is not None:
+            layout.addWidget(self)
+
+        # add a label to this layout
+        self.label = QtWidgets.QLabel(name)
+        self.layout().addWidget(self.label)
+
+    def setLabel(self, text):
+        # update the label
+        self.label.setText(text)
+
+    def emitSignal(self):
+        if self.value() != self.last_emited_value:
+            self.valueChanged.emit(self.value())
+            self.last_emited_value = self.value()
+
+    def valueChangedEvent(self, value):
+        if self.no_signal:
+            return
+        self.setValue(value)
+        self.emitSignal()
+
+    def setValue(self, value):
+        self.no_signal = True
+        try:
+            self.doSetValue(value)
+        finally:
+            self.no_signal = False
+
+    def doSetValue(self, value):
+        # dummy method to be overloaded by child classes
+        pass
+
+    def value(self):
+        # dummy method to be overloaded by child classes
+        pass
+
+
+class QInputNumber(QInput):
+    slider_dragged = False
+
+    def __init__(self, layout=None, name=None, value=0, min=None, max=None, use_slider=False, float=True, decimals=2,
+                 unit=None):
+        # initialize the super widget
+        QInput.__init__(self, layout, name)
+
+        if float is False:
+            self.decimals = 1
+        else:
+            self.decimals = decimals
+
+        if use_slider and min is not None and max is not None:
+            # slider
+            self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+            self.layout().addWidget(self.slider)
+            self.slider.setRange(min * self.decimals, max * self.decimals)
+            self.slider.valueChanged.connect(lambda x: self.valueChangedEvent(x / self.decimals))
+            self.slider.sliderPressed.connect(lambda: self.setSliderDragged(True))
+            self.slider.sliderReleased.connect(lambda: self.setSliderDragged(False))
+        else:
+            self.slider = None
+
+        # add spin box
+        if float:
+            self.spin_box = QtWidgets.QDoubleSpinBox()
+            self.spin_box.setDecimals(decimals)
+        else:
+            self.spin_box = QtWidgets.QSpinBox()
+        if unit is not None:
+            self.spin_box.setSuffix(" " + unit)
+        self.layout().addWidget(self.spin_box)
+        self.spin_box.valueChanged.connect(self.valueChangedEvent)
+
+        if min is not None:
+            self.spin_box.setMinimum(min)
+        if max is not None:
+            self.spin_box.setMaximum(max)
+
+        self.setValue(value)
+
+    def setSliderDragged(self, value):
+        self.slider_dragged = value
+        if value is False:
+            self.emitSignal()
+
+    def valueChangedEvent(self, value):
+        if self.no_signal:
+            return
+        self.setValue(value)
+        if not self.slider_dragged:
+            self.emitSignal()
+
+    def doSetValue(self, value):
+        self.spin_box.setValue(value)
+        if self.slider is not None:
+            self.slider.setValue(value * self.decimals)
+
+    def value(self):
+        return self.spin_box.value()
+
+
+class QInputString(QInput):
+
+    def __init__(self, layout=None, name=None, value=""):
+        # initialize the super widget
+        QInput.__init__(self, layout, name)
+
+        self.line_edit = QtWidgets.QLineEdit()
+        self.layout().addWidget(self.line_edit)
+        self.line_edit.editingFinished.connect(lambda: self.valueChangedEvent(self.value()))
+
+        self.setValue(value)
+
+    def doSetValue(self, value):
+        self.line_edit.setText(value)
+
+    def value(self):
+        return self.line_edit.text()
+
+
+class QInputBool(QInput):
+
+    def __init__(self, layout=None, name=None, value=False):
+        # initialize the super widget
+        QInput.__init__(self, layout, name)
+
+        self.checkbox = QtWidgets.QCheckBox()
+        self.layout().addWidget(self.checkbox)
+        self.checkbox.stateChanged.connect(lambda: self.valueChangedEvent(self.value()))
+
+        self.setValue(value)
+
+    def doSetValue(self, value):
+        self.checkbox.setChecked(value)
+
+    def value(self):
+        return self.checkbox.isChecked()
+
+
+class QInputChoice(QInput):
+
+    def __init__(self, layout=None, name=None, value=None, values=None):
+        # initialize the super widget
+        QInput.__init__(self, layout, name)
+
+        self.combobox = QtWidgets.QComboBox()
+        self.layout().addWidget(self.combobox)
+        self.combobox.currentIndexChanged.connect(lambda: self.valueChangedEvent(self.value()))
+
+        self.values = values
+        self.combobox.addItems(values)
+
+        self.setValue(value)
+
+    def doSetValue(self, value):
+        self.combobox.setCurrentIndex(self.values.index(value))
+
+    def value(self):
+        return self.values[self.combobox.currentIndex()]
+
+
+class QInputColor(QInput):
+
+    def __init__(self, layout=None, name=None, value=None):
+        # initialize the super widget
+        QInput.__init__(self, layout, name)
+
+        self.button = QtWidgets.QPushButton()
+        self.button.setMaximumWidth(40)
+        self.layout().addWidget(self.button)
+        self.button.clicked.connect(self.openDialog)
+
+        # set the color
+        self.setValue(value)
+
+    def changeEvent(self, event):
+        if event.type() == QtCore.QEvent.EnabledChange:
+            if not self.isEnabled():
+                self.button.setStyleSheet("background-color: #f0f0f0;")
+            else:
+                self.setValue(self.color)
+
+    def openDialog(self):
+        # get new color from color picker
+        color = QtWidgets.QColorDialog.getColor(QtGui.QColor(*tuple(mpl.colors.to_rgba_array(self.value())[0] * 255)),
+                                                self.parent(), self.label.text() + " choose color")
+        # if a color is set, apply it
+        if color.isValid():
+            color = mpl.colors.to_hex(color.getRgbF())
+            self.setValue(color)
+            self.emitSignal()
+
+    def setValue(self, value):
+        # display and save the new color
+        if value is None:
+            value = "#FF0000FF"
+        self.button.setStyleSheet("background-color: %s;" % value)
+        self.color = value
+
+    def value(self):
+        # return the color
+        return self.color
+
 
 def AddQSpinBox(layout, text, value=0, float=True, strech=False):
     horizontal_layout = QtWidgets.QHBoxLayout()
@@ -37,9 +261,11 @@ def AddQSpinBox(layout, text, value=0, float=True, strech=False):
     spinBox.setRange(-99999, 99999)
     spinBox.setValue(value)
     spinBox.setHidden_ = spinBox.setHidden
+
     def setHidden(hidden):
         spinBox.setHidden_(hidden)
         text.setHidden(hidden)
+
     spinBox.setHidden = setHidden
     horizontal_layout.addWidget(text)
     horizontal_layout.addWidget(spinBox)
@@ -67,7 +293,8 @@ def AddQLineEdit(layout, text, value=None, strech=False, editwidth=None):
     return lineEdit
 
 
-def AddQSaveFileChoose(layout, text, value=None, dialog_title="Choose File", file_type="All", filename_checker=None, strech=False):
+def AddQSaveFileChoose(layout, text, value=None, dialog_title="Choose File", file_type="All", filename_checker=None,
+                       strech=False):
     horizontal_layout = QtWidgets.QHBoxLayout()
     layout.addLayout(horizontal_layout)
     text = QtWidgets.QLabel(text)
@@ -76,6 +303,7 @@ def AddQSaveFileChoose(layout, text, value=None, dialog_title="Choose File", fil
         lineEdit.setText(value)
     lineEdit.label = text
     lineEdit.setEnabled(False)
+
     def OpenDialog():
         srcpath = QtWidgets.QFileDialog.getSaveFileName(None, dialog_title, os.getcwd(), file_type)
         if isinstance(srcpath, tuple):
@@ -86,6 +314,7 @@ def AddQSaveFileChoose(layout, text, value=None, dialog_title="Choose File", fil
             srcpath = filename_checker(srcpath)
         if srcpath:
             lineEdit.setText(srcpath)
+
     button = QtWidgets.QPushButton("Choose File")
     button.pressed.connect(OpenDialog)
     horizontal_layout.addWidget(text)
@@ -96,7 +325,9 @@ def AddQSaveFileChoose(layout, text, value=None, dialog_title="Choose File", fil
         horizontal_layout.addStretch()
     return lineEdit
 
-def AddQOpenFileChoose(layout, text, value=None, dialog_title="Choose File", file_type="All", filename_checker=None, strech=False):
+
+def AddQOpenFileChoose(layout, text, value=None, dialog_title="Choose File", file_type="All", filename_checker=None,
+                       strech=False):
     horizontal_layout = QtWidgets.QHBoxLayout()
     layout.addLayout(horizontal_layout)
     text = QtWidgets.QLabel(text)
@@ -105,6 +336,7 @@ def AddQOpenFileChoose(layout, text, value=None, dialog_title="Choose File", fil
         lineEdit.setText(value)
     lineEdit.label = text
     lineEdit.setEnabled(False)
+
     def OpenDialog():
         srcpath = QtWidgets.QFileDialog.getOpenFileName(None, dialog_title, os.getcwd(), file_type)
         if isinstance(srcpath, tuple):
@@ -115,6 +347,7 @@ def AddQOpenFileChoose(layout, text, value=None, dialog_title="Choose File", fil
             srcpath = filename_checker(srcpath)
         if srcpath:
             lineEdit.setText(srcpath)
+
     button = QtWidgets.QPushButton("Choose File")
     button.pressed.connect(OpenDialog)
     horizontal_layout.addWidget(text)
@@ -124,6 +357,7 @@ def AddQOpenFileChoose(layout, text, value=None, dialog_title="Choose File", fil
     if strech:
         horizontal_layout.addStretch()
     return lineEdit
+
 
 def AddQColorChoose(layout, text, value=None, strech=False):
     # add a layout
@@ -180,12 +414,14 @@ def AddQComboBox(layout, text, values=None, selectedValue=None):
     if selectedValue:
         comboBox.setCurrentIndex(values.index(selectedValue))
     comboBox.values = values
+
     def setValues(new_values):
         for i in range(len(comboBox.values)):
             comboBox.removeItem(0)
         for value in new_values:
             comboBox.addItem(value)
         comboBox.values = new_values
+
     comboBox.setValues = setValues
     horizontal_layout.addWidget(text)
     horizontal_layout.addWidget(comboBox)
@@ -226,17 +462,19 @@ def GetColorByIndex(index):
     colors = np.linspace(0, 1, 16, endpoint=False).tolist() * 3  # 16 different hues
     saturations = [1] * 16 + [0.5] * 16 + [1] * 16  # in two different saturations
     value = [1] * 16 + [1] * 16 + [0.5] * 16  # and two different values
-    return "#%02x%02x%02x" % tuple((np.array(colorsys.hsv_to_rgb((np.array(colors[index])*3) % 1, saturations[index], value[index])) * 255).astype(int))
+    return "#%02x%02x%02x" % tuple((np.array(
+        colorsys.hsv_to_rgb((np.array(colors[index]) * 3) % 1, saturations[index], value[index])) * 255).astype(int))
+
 
 # set the standard colors for the color picker dialog
-colors = np.linspace(0, 1, 16, endpoint=False).tolist()*3  # 16 different hues
-saturations = [1]*16+[0.5]*16+[1]*16  # in two different saturations
-value = [1]*16+[1]*16+[0.5]*16  # and two different values
+colors = np.linspace(0, 1, 16, endpoint=False).tolist() * 3  # 16 different hues
+saturations = [1] * 16 + [0.5] * 16 + [1] * 16  # in two different saturations
+value = [1] * 16 + [1] * 16 + [0.5] * 16  # and two different values
 for index, (color, sat, val) in enumerate(zip(colors, saturations, value)):
     # deform the index, as the dialog fills them column wise and we want to fill them row wise
-    index = index % 8*6+index//8
+    index = index % 8 * 6 + index // 8
     # convert color from hsv to rgb, to an array, to an tuple, to a hex string then to an integer
-    color_integer = int("%02x%02x%02x" % tuple((np.array(colorsys.hsv_to_rgb(color, sat, val))*255).astype(int)), 16)
+    color_integer = int("%02x%02x%02x" % tuple((np.array(colorsys.hsv_to_rgb(color, sat, val)) * 255).astype(int)), 16)
     try:
         QtWidgets.QColorDialog.setStandardColor(index, QtGui.QColor(color_integer))  # for Qt5
     except TypeError:
