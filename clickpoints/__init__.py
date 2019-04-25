a583392de902da4fa8e1950e9b2742e84de81f38
Randrian <Randrian@localhost>
2019-04-25 15:14:24 +0200

    set version to 1.8.0

--- clickpoints/__init__.py
+++ clickpoints/__init__.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with ClickPoints. If not, see <http://www.gnu.org/licenses/>
 
-__version__ = '1.7.0'
+__version__ = '1.8.0'
 
 from .Addon import Addon
 from .DataFile import DataFile, MaskDtypeMismatch, MaskDimensionMismatch, MaskDimensionUnknown
