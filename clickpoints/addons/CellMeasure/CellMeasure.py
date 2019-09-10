3d929cc277e196ce789fc94ccd29bdeb7e04f568
Alex <55278900+AlexanderWinterl@users.noreply.github.com>
2019-09-10 19:27:31 +0200

    * addons/CellMeasure: Added segmentation for microfluidic, added filters for MOST regioprops properties.

--- clickpoints/addons/CellMeasure/CellMeasure.py
+++ clickpoints/addons/CellMeasure/CellMeasure.py
@@ -34,9 +34,67 @@ from skimage.color import rgb2grey
 from scipy.ndimage.morphology import binary_erosion, binary_fill_holes
 from scipy.ndimage.filters import gaussian_filter
 from skimage.filters import threshold_otsu, gaussian, threshold_local
-from skimage.morphology import binary_closing, remove_small_objects
+from skimage.morphology import binary_closing, remove_small_objects, opening, disk
+from skimage.feature import canny
 import pandas as pd
 from imageio import imread
+
+REGIONPROPS_TYPE = {"area": "int",
+                    "bbox_area": "int",
+                    "convex_area": "int",
+                    "eccentricity": "float",
+                    "equivalent_diameter": "float",
+                    "euler_number": "int",
+                    "extent": "float",
+                    "filled_area": "int",
+                    "label": "int",
+                    "major_axis_length": "float",
+                    "max_intensity": "float",
+                    "mean_intensity": "float",
+                    "min_intensity": "float",
+                    "minor_axis_length": "float",
+                    "orientation": "float",
+                    "perimeter": "float",
+                    "solidity": "float"
+                    }
+REGIONPROPS_MINMAXDEF = {"area": [0, 10000, 200, 1000],
+                         "bbox_area": [0, 10000, 200, 1000],
+                         "convex_area": [0, 10000, 200, 1000],
+                         "eccentricity": [0., 1., 0.1, 0.9],
+                         "equivalent_diameter": [0, 1000, 0, 10],
+                         "euler_number": [-100, 100, -100, 100],
+                         "extent": [0, 1, 0.1, 0.9],
+                         "filled_area": [0, 10000, 200, 1000],
+                         "label": [0, 10000, 200, 1000],
+                         "major_axis_length": [0., 1000., 0., 10.],
+                         "max_intensity": [0., 255., 10., 230.],
+                         "mean_intensity": [0., 255., 10., 230.],
+                         "min_intensity": [0., 255., 10., 230.],
+                         "minor_axis_length": [0., 1000., 0., 10.],
+                         "orientation": [-np.pi / 2, np.pi / 2, -np.pi / 2, np.pi / 2],
+                         "perimeter": [0., 1000., 0., 10.],
+                         "solidity": [0., 1., 0.1, 0.9]
+                         }
+REGIONPROPS_HELP = {"area": "Number of pixels of region.",
+                    "bbox_area": "Number of pixels of bounding box.",
+                    "convex_area": "Number of pixels of convex hull image. ",
+                    "eccentricity": "Eccentricity of the ellipse that has the same second-moments as the region. The eccentricity is the ratio of the focal distance (distance between focal points) over the major axis length. The value is in the interval [0, 1). When it is 0, the ellipse becomes a circle. ",
+                    "equivalent_diameter": "The diameter of a circle with the same area as the region. ",
+                    "euler_number": "Euler characteristic of region. Computed as number of objects (= 1) subtracted by number of holes (8-connectivity). ",
+                    "extent": "Ratio of pixels in the region to pixels in the total bounding box. Computed as ``area / (rows * cols)`` ",
+                    "filled_area": "Number of pixels of filled region. ",
+                    "label": "The label in the labeled input image. ",
+                    "major_axis_length": "The length of the major axis of the ellipse that has the same normalized second central moments as the region. ",
+                    "max_intensity": "Value with the greatest intensity in the region. ",
+                    "mean_intensity": "Value with the mean intensity in the region. ",
+                    "min_intensity": "Value with the least intensity in the region. ",
+                    "minor_axis_length": "The length of the minor axis of the ellipse that has the same normalized second central moments as the region. ",
+                    "orientation": "In 'rc' coordinates, angle between the 0th axis (rows) and the major axis of the ellipse that has the same second moments as the region, ranging from `-pi/2` to `pi/2` counter-clockwise. In `xy` coordinates, as above but the angle is now measured from the 'x' or horizontal axis. ",
+                    "perimeter": "Perimeter of object which approximates the contour as a line through the centers of border pixels using a 4-connectivity. ",
+                    "solidity": "Ratio of pixels in the region to pixels of the convex hull image."
+                    }
+
+
 
 
 
@@ -70,7 +128,7 @@ class Addon(clickpoints.Addon):
         self.addOption(key="segmentation_invert_mask", display_name="Invert Mask", default=False, value_type="bool",
                        tooltip="If true, inverts the mask, so that darker areas are marked instead of brighter ones.")
         self.addOption(key="segmentation_mode", display_name="Segmentation Mode", default="th_simple", value_type="choice_string",
-                       values=["th_simple","th_local", "th_lukas"], tooltip="segmentation approach")
+                       values=["th_simple","th_local", "th_lukas", "th_elham"], tooltip="segmentation approach")
 
         # preprocessing
         self.addOption(key="segmentation_gauss", display_name="Gauss Sigma\t", default=1.25, value_type="float",
@@ -83,6 +141,17 @@ class Addon(clickpoints.Addon):
                        min_value=0, max_value=200, tooltip="Block radius used for local thresholding")
         self.addOption(key="segmentation_localth_min_size", display_name="Min object size", default=31, value_type="int",
                        min_value=0, max_value=100, tooltip="Objects below this size will be removed")
+
+        # th_elham
+        self.addOption(key="segmentation_EL_min_size", display_name="Min object size", default=1, value_type="int",
+                       min_value=1, max_value=10, tooltip="Size of the DISK shaped element for the binary open operation")
+        self.addOption(key="segmentation_EL_max_size", display_name="Max object size", default=1, value_type="int",
+                       min_value=1, max_value=10, tooltip="Size of the DISK shaped element for the binary open operation")
+        self.addOption(key="segmentation_EL_low_th", display_name="Lower Canny threshold", default=0.9, value_type="float",
+                       min_value=0, max_value=1, tooltip="Minimal value for canny clipping")
+        self.addOption(key="segmentation_EL_high_th", display_name="Upper Canny threshold", default=0.99, value_type="float",
+                       min_value=0, max_value=1, tooltip="Minimal value for canny clipping")
+
         # post processing
         self.addOption(key="segmentation_slm_size", display_name="opening DISK size", default=2, value_type="int",
                        min_value=0, max_value=10, tooltip="Size of the DISK shaped element for the binary open operation")
@@ -93,15 +162,32 @@ class Addon(clickpoints.Addon):
         ## evaluation
         self.addOption(key="evaluation_layer", display_name="Evaluation Layer", default=layers[0], value_type="choice_string",
                        values=layers, tooltip="The layer on which to evaluate the mask",)
-        self.addOption(key="evaluation_min_area", display_name="Min Area", default=200, value_type="int",
-                       min_value=0, max_value=100000, tooltip="Exclude all patches with areas smaller than this value.")
+
+        self.regionsprops_actOptions = {}
+        self.regionsprops_minOptions = {}
+        self.regionsprops_maxOptions = {}
+        for key in REGIONPROPS_TYPE:
+            self.addOption(key="evaluation_act_%s"%(key), display_name=key, default=False, value_type="bool",
+                           tooltip="Use %s to filter output\n%s : %s"%(key, key, REGIONPROPS_HELP[key]))
+            self.regionsprops_actOptions[key] = "evaluation_act_%s"%(key)
+            self.addOption(key="evaluation_min_%s"%(key), display_name="Min %s"%key, default=REGIONPROPS_MINMAXDEF[key][2],
+                           value_type=REGIONPROPS_TYPE[key], min_value=REGIONPROPS_MINMAXDEF[key][0], max_value=REGIONPROPS_MINMAXDEF[key][1],
+                           tooltip="%s :\n %s"%(key, REGIONPROPS_HELP[key]))
+            self.regionsprops_minOptions[key] = "evaluation_min_%s"%(key)
+            self.addOption(key="evaluation_max_%s"%(key), display_name="Max %s"%key, default=REGIONPROPS_MINMAXDEF[key][3],
+                           value_type=REGIONPROPS_TYPE[key], min_value=REGIONPROPS_MINMAXDEF[key][0], max_value=REGIONPROPS_MINMAXDEF[key][1],
+                           tooltip="%s :\n %s"%(key, REGIONPROPS_HELP[key]))
+            self.regionsprops_maxOptions[key] = "evaluation_max_%s"%(key)
+        self.regionsprops_Options = list(self.regionsprops_actOptions.values()) + list(self.regionsprops_maxOptions.values()) + list(self.regionsprops_minOptions.values())
 
         # define options groups for unified callback handling, e.g only call an update if parameter of this group was changed
         self.options_Segmentation = ['segmentation_th', 'segmentation_slm_size','segmentation_gauss','segmentation_invert_mask','segmentation_layer',
-                                     'segmentation_localth_block_size', 'segmentation_localth_min_size']
-        self.options_Output = ['evaluation_min_area', 'evaluation_layer']
+                                     'segmentation_localth_block_size', 'segmentation_localth_min_size',
+                                     "segmentation_EL_min_size", "segmentation_EL_max_size","segmentation_EL_low_th","segmentation_EL_high_th"]
 
+        self.options_Output = ['evaluation_min_area', 'evaluation_layer'] + self.regionsprops_Options
 
+        self.addOption(key="batchsegment", display_name="Batch Segment Images", default=False, value_type="bool", tooltip="Create new masks for images while batch processing them.")
         """ Setup Marker and Masks """
         # Check if the marker type is present
         self.db.setMarkerType("cell (auto)", [0, 255, 255], self.db.TYPE_Normal)
@@ -120,7 +206,7 @@ class Addon(clickpoints.Addon):
         self.setWindowTitle("Cell Measurement")
         self.setWindowIcon(qta.icon("fa.compress"))
         self.setMinimumWidth(400)
-        self.setMinimumHeight(200)
+        self.setMinimumHeight(900)
         self.layout = QtWidgets.QVBoxLayout(self)
 
         """ groupbox SEGMENTATION"""
@@ -148,6 +234,12 @@ class Addon(clickpoints.Addon):
         self.sliderBlocksize = self.inputOption("segmentation_localth_block_size", self.segmentation_layout, use_slider=True)
         self.sliderMinsize = self.inputOption("segmentation_localth_min_size", self.segmentation_layout, use_slider=True)
 
+        # Mode th_elham
+        self.sliderELMinsize = self.inputOption("segmentation_EL_min_size", self.segmentation_layout, use_slider=True)
+        self.sliderELMaxsize = self.inputOption("segmentation_EL_max_size", self.segmentation_layout, use_slider=True)
+        self.sliderELLowThreshold = self.inputOption("segmentation_EL_low_th", self.segmentation_layout, use_slider=True)
+        self.sliderELHighThreshold = self.inputOption("segmentation_EL_high_th", self.segmentation_layout, use_slider=True)
+
         self.segmentation_layout.addWidget(QtWidgets.QLabel("<b>Post-Processing</b>"))
         self.sliderSelemSize = self.inputOption("segmentation_slm_size", self.segmentation_layout, use_slider=True)
 
@@ -156,7 +248,8 @@ class Addon(clickpoints.Addon):
 
         self.segmode_simpleTH = [self.sliderSegmentationTH ]
         self.segmode_localTH = [self.sliderBlocksize, self.sliderMinsize]
-        self.segmode_all = self.segmode_simpleTH + self.segmode_localTH
+        self.segmode_elhamTH = [self.sliderELMinsize, self.sliderELMaxsize, self.sliderELLowThreshold, self.sliderELHighThreshold]
+        self.segmode_all = self.segmode_simpleTH + self.segmode_localTH + self.segmode_elhamTH
 
         # segment on button press
         self.buttonSegmentation = QtWidgets.QPushButton("Segment")
@@ -172,7 +265,17 @@ class Addon(clickpoints.Addon):
         self.position_groupbox.setLayout(self.position_layout)
 
         self.evaluationLayer = self.inputOption("evaluation_layer", self.position_layout)
-        self.inputMinSize = self.inputOption("evaluation_min_area", self.position_layout, use_slider=True)
+
+        self.evalInputs = {}
+        self.evalLayouts = {}
+        for i, key in enumerate(REGIONPROPS_TYPE):
+            self.evalLayouts[key] = QtWidgets.QHBoxLayout()
+            self.position_layout.addLayout(self.evalLayouts[key])
+            self.evalInputs[key] = [
+                self.inputOption(self.regionsprops_actOptions[key], self.evalLayouts[key]),
+                self.inputOption(self.regionsprops_minOptions[key], self.evalLayouts[key], use_slider=True),
+                self.inputOption(self.regionsprops_maxOptions[key], self.evalLayouts[key], use_slider=True)]
+
 
         # button Properties
         self.position_layout2 = QtWidgets.QHBoxLayout()
@@ -198,14 +301,17 @@ class Addon(clickpoints.Addon):
         self.batch_groupbox.setLayout(self.batch_layout)
 
         self.buttonProcAll = QtWidgets.QPushButton('Process all Images')
+        self.buttonSegAll = self.inputOption("batchsegment", self.batch_layout)
         self.batch_layout.addWidget(self.buttonProcAll)
         self.buttonProcAll.released.connect(self.processAll)
 
         """ update view """
         self.updateSegmentationMode()
+        self.updateFilterMode()
 
     def optionsChanged(self, key):
         print("optionsChanged:\t",key)
+        print(key in self.regionsprops_actOptions.values())
         if key in self.options_Segmentation and self.getOption("segmentation_auto_apply"):
             self.updateSegmentation()
         elif key in self.options_Output:
@@ -213,6 +319,8 @@ class Addon(clickpoints.Addon):
         elif key == 'segmentation_mode':
             print("Mode changed to ", self.getOption("segmentation_mode"))
             self.updateSegmentationMode()
+        if key in self.regionsprops_actOptions.values():
+            self.updateFilterMode()
 
     def updateSegmentationMode(self):
         """
@@ -233,6 +341,18 @@ class Addon(clickpoints.Addon):
             _ = [ e.setVisible(False) for e in self.segmode_all]
             _ = [ e.setVisible(True) for e in self.segmode_simpleTH]
 
+        elif self.getOption("segmentation_mode") == "th_elham":
+            _ = [ e.setVisible(False) for e in self.segmode_all]
+            _ = [ e.setVisible(True) for e in self.segmode_elhamTH]
+
+    def updateFilterMode(self):
+        for key in self.regionsprops_actOptions:
+            if self.getOption(self.regionsprops_actOptions[key]):
+                self.evalInputs[key][1].setVisible(True)
+                self.evalInputs[key][2].setVisible(True)
+            else:
+                self.evalInputs[key][1].setVisible(False)
+                self.evalInputs[key][2].setVisible(False)
 
 
 
@@ -311,10 +431,16 @@ class Addon(clickpoints.Addon):
             img_otsu = (img_gaus > otsu_th) + 0
             mask = (binary_fill_holes(img_otsu) + 0).astype('uint8')
 
+        if mode == "th_elham":
+            struct = disk(self.getOption("segmentation_EL_min_size"))#structural element for binary erosion
+            # "segmentation_EL_min_size", "segmentation_EL_max_size","segmentation_EL_low_th","segmentation_EL_high_th"
+            mask = canny(img, sigma=1, low_threshold=self.getOption("segmentation_EL_low_th"), high_threshold=self.getOption("segmentation_EL_high_th"),use_quantiles=True)  # edge detection
+            print(mask)
+            mask = binary_fill_holes(mask, structure=struct) #fill holes
+            mask = binary_erosion(mask, structure=struct).astype(np.uint8)  # erode to remove lines and small schmutz
 
         # use open operation to reduce noise
         if self.getOption("segmentation_slm_size") != 0:
-            from skimage.morphology import opening, disk
             mask_open = opening(mask, disk(self.getOption("segmentation_slm_size")))
         else:
             mask_open = mask
@@ -385,20 +511,33 @@ class Addon(clickpoints.Addon):
 
         # get regionprops
         mask_labeled = label(mask)
-        props = regionprops(mask_labeled, img)
+        regions = regionprops(mask_labeled, img)
 
         # iterate over properties and set marker for display
-        for nr,prop in enumerate(props):
-            if prop.area > self.inputMinSize.value():
-                self.db.setMarker(image=self.qimg, x=prop.centroid[1], y=prop.centroid[0], text='Cell %d\narea= %.2f' % (nr,prop.area), type='cell (auto)')
+        passed = {}
+        for nr,region in enumerate(regions):
+            passed[nr] = True
+            text = ""
+            for key in self.regionsprops_actOptions:
+                if self.getOption(self.regionsprops_actOptions[key]):
+                    if self.evalInputs[key][1].value()<=region[key]<=self.evalInputs[key][2].value():
+                        text += "\n%s= %.2f"%(key, region[key])
+                        pass
+                    else:
+                        passed[nr] = False
+                        break
+            if passed[nr]:
+                self.db.setMarker(image=self.qimg, x=region.centroid[1], y=region.centroid[0],
+                                  text='Cell %d%s' % (nr, text), type='cell (auto)')
 
         # update CP display to show marker
         self.cp.reloadMarker()
 
         # store parameters in Pandas DF
         results = []
-        for nr, prop in enumerate(props):
-            if prop.area > self.inputMinSize.value():
+        for nr, prop in enumerate(regions):
+            # if prop.area > self.inputMinSize.value():
+            if passed[nr]:
                 tmp = dotdict()
                 tmp.filename = self.qimg.filename
                 tmp.nr = nr
@@ -447,9 +586,11 @@ class Addon(clickpoints.Addon):
         results = []
         for q_img in q_images:
             self.q_img = q_img
+            self.cp.jumpToFrameWait(q_img.sort_index)
             print("Batch processing Image Nr %d" % q_img.sort_index)
 
-            # self.updateSegmentation(qimg=q_img)
+            if self.getOption("batchsegment"):
+                self.updateSegmentation(qimg=q_img)
             df = self.updateProperties(qimg=q_img)
             results.append(df)
 
