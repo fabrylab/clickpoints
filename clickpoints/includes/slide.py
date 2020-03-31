11a588df7c5bbd94d50393b18b60f6a4c1f666ec
AlexanderWinterl <alexander.winterl@fau.de>
2020-03-31 14:01:48 +0200

    fixed 'missing tables'-bug from tif pages. Now able to read jpeg compressed tif tiles

--- clickpoints/includes/slide.py
+++ clickpoints/includes/slide.py
@@ -52,8 +52,10 @@ def read_crop_of_page(page: tifffile.TiffPage, loc: Tuple[int, int], size: Tuple
     # decode the tiles
     fh = page.parent.filehandle
     segmentiter = fh.read_segments(used_offsets, used_bytecounts)
+    decodeargs = {}
+    decodeargs["tables"] = page._gettags({347}, lock=None)[0][1].value
     for seg, i in zip(segmentiter, slide_indices_shapes):
-        segment, indices, shape = page.decode(seg[0], i)
+        segment, indices, shape = page.decode(seg[0], i, **decodeargs)
         result[indices[3]-sx1:indices[3]+shape[1]-sx1, indices[4]-sy1:indices[4]+shape[2]-sy1] = segment
 
     # optionally drop the channel dimension
