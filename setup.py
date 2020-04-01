e2cf06fc7dea153828c4615c70976f919c593ef0
AlexanderWinterl <alexander.winterl@fau.de>
2020-04-01 19:53:20 +0200

    Revert "set version to v1.9.4"
    This reverts commit 00ab765fddfcb277468c777da21836095737d4ac.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 
 setup(name='clickpoints',
-      version="1.9.4",
+      version="1.9.3",
       description='Scientific toolbox for manual and automatic image evaluation.',
       long_description=open('README.rst').read(),
       url='https://github.com/fabrylab/clickpoints',
