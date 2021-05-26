40c020f93e9fde9c039372014cca35c894ac89c2
unknown <alexander.winterl@fau.de>
2021-05-26 10:14:53 +0200

    set version to v1.9.9

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 
 setup(name='clickpoints',
-      version="1.9.8",
+      version="1.9.9",
       description='Scientific toolbox for manual and automatic image evaluation.',
       long_description=open('README.rst').read(),
       url='https://github.com/fabrylab/clickpoints',
