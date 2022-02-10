51ad5fdfb6368e3261a317f2f848a4a84c1ba782
rgerum <14153051+rgerum@users.noreply.github.com>
2022-02-10 16:56:09 -0500

    set version to v1.9.11

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 
 setup(name='clickpoints',
-      version="1.9.10",
+      version="1.9.11",
       description='Scientific toolbox for manual and automatic image evaluation.',
       long_description=open('README.rst').read(),
       url='https://github.com/fabrylab/clickpoints',
