81fd1074cb4ff40208ba271b717690749d629907
rgerum <14153051+rgerum@users.noreply.github.com>
2022-11-10 09:45:52 -0500

    added reference to refs.bi to docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,7 @@ extensions = [
     'nbsphinx',
     'sphinxcontrib.bibtex'
 ]
+bibtex_bibfiles = ['refs.bib']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
